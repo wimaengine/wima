fe2a2a6c38d90e13018dac2adbda9505ff21d64c
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2024-04-14 07:22:12 +0300

    [ecs] Added `TimeoutExecutor` which executes once with a delay.

---
+++ src/ecs/schedule/executors/timeout.js
@@ -0,0 +1,26 @@
+import { World } from '../../registry.js'
+import { Schedule } from '../schedule.js'
+import { Executor } from './executor.js'
+
+/**
+ * This executor executes once with a given delay after it is 
+ * {@link TimeoutExecutor.start started}.
+ * 
+ * This is a wrapper for {@link  https://developer.mozilla.org/en-US/docs/Web/API/setTimeout `setTimeout`}.
+ */
+export class TimeoutExecutor extends Executor {
+  constructor(delay = 0){
+    super(delay)
+  }
+
+  /**
+   * @param {World} world
+   * @param {Schedule} schedule
+   */
+  start(world, schedule) {
+    this.tick = setTimeout(() => { schedule.run(world) }, this.time)[Symbol.toPrimitive]()
+  }
+  stop() {
+    clearTimeout(this.tick)
+  }
+}
