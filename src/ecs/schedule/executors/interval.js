804ff85057caa66fa320832127f6c65c54afe381
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2024-04-16 07:22:12 +0300

    [ecs] Added `IntervalExecutor` which executes every given duration.

---
+++ src/ecs/schedule/executors/interval.js
@@ -0,0 +1,30 @@
+import { Executor } from './executor.js'
+import { World } from '../../registry.js'
+import { Schedule } from '../schedule.js'
+
+/**
+ * This executor executes every given duration after it is 
+ * {@link IntervalExecutor.start started}.
+ * 
+ * This is a wrapper for {@link  https://developer.mozilla.org/en-US/docs/Web/API/setInterval `setInterval`}.
+ */
+export class IntervalExecutor extends Executor {
+
+  /**
+   * @param {number} duration 
+   */
+  constructor(duration = 0){
+    super(duration)
+  }
+
+  /**
+   * @param {World} world
+   * @param {Schedule} schedule
+   */
+  start(world, schedule) {
+    this.tick = setInterval(() => { schedule.run(world) }, this.time)[Symbol.toPrimitive]()
+  }
+stop() {
+    clearInterval(this.tick)
+  }
+}
