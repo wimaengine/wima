b584c2bf005e97f0da5b79001b0cd7944d5ac5bf
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2024-05-02 14:24:32 +0300

    [ecs] Added `Query` which does operations on supplied components on a given `World`.

---
+++ src/ecs/query.js
@@ -0,0 +1,206 @@
+/** @import { ComponentId, ArchetypeId, Entity, Tuple, ComponentName } from './typedef/index.js'*/
+import { World } from './registry.js'
+import { ArchetypeTable } from './tables/archetypetable.js'
+
+/**
+ * Enables operations to be performed on specified set 
+ * of components components on a {@link World}.
+ * Ensure that the component types matches up with the 
+ * component names given in the second parameter of 
+ * the query in lower case.
+ * @example
+ * ```ts
+ * class A {}
+ * class B {}
+ * 
+ * const world = new World()
+ *   .registerType(A)
+ *   .registerType(B)
+ * const query = new Query<[A, B]>(world, ['a','b'])
+ * 
+ * // you can now use the query to perform operations
+ * // on entities with components `A` and `B`
+ * // see the {@link Query} methods to know what operations 
+ * // are available
+ * ```
+ * 
+ * @template {Tuple} T
+ */
+export class Query {
+
+  /**
+   * @private
+   * @type {World}
+   */
+  registry
+
+  /**
+   * @readonly
+   * @type {ComponentId[]}
+   */
+  descriptors = []
+
+  /**
+   * @private
+   * @type {any[][]}
+   */
+  components = []
+
+  /**
+   * @private
+   * @type {Map<ArchetypeId,number>}
+   */
+  archmapper = new Map()
+
+  /**
+   * @param {World} registry
+   * @param {ComponentName[]} descriptors
+   */
+  constructor(registry, descriptors) {
+    this.registry = registry
+
+    for (let i = 0; i < descriptors.length; i++) {
+      this.components[i] = []
+    }
+
+    this.descriptors = registry.getComponentIdsByName(descriptors)
+    this.update(registry.getTable())
+  }
+
+  /**
+   * @param {ArchetypeTable} table
+   */
+  update(table) {
+    const { descriptors, components } = this
+    const archids = table.getArchetypeIds(descriptors, [])
+
+    for (let i = 0; i < archids.length; i++) {
+      this.archmapper.set(archids[i], i)
+    }
+
+    // This will help implement query filters
+    // const archetypes = table.filterArchetypes((archetype)=>true)
+    for (let i = 0; i < descriptors.length; i++) {
+      for (let j = 0; j < archids.length; j++) {
+
+        // instead of keeping the component lists,keep the verified archetype
+        // as their ids to get them later
+        const bin = table.getArchetype(archids[j]).components.get(descriptors[i])
+
+        components[i].push(bin)
+      }
+    }
+  }
+
+  /**
+   * Gets the components of a given entity.
+   * 
+   * @param {Entity} entity
+   * @returns {T | null}
+   */
+  get(entity) {
+    const entities = this.registry.getEntities()
+    const tableid = this.archmapper.get(
+      entities[entity]
+    )
+
+    if (tableid === undefined) return null
+
+    const index = entities[entity + 1]
+    const components = new Array(this.descriptors.length)
+
+    for (let i = 0; i < this.descriptors.length; i++) {
+      components[i] = this.components[i][tableid][index]
+    }
+
+    // @ts-ignore
+    // SAFETY: Components are type cast to the expected return value
+    return components
+  }
+  
+  /**
+   * @param {EachFunc<T>} callback
+   */
+  each(callback) {
+    const components = new Array(this.descriptors.length)
+
+    if (!this.components[0]) return
+
+    for (let j = 0; j < this.components[0].length; j++) {
+      for (let k = 0; k < this.components[0][j].length; k++) {
+        for (let l = 0; l < this.descriptors.length; l++) {
+          components[l] = this.components[l][j][k]
+        }
+
+        // @ts-ignore
+        // SAFETY: Components are type cast to the expected passed value
+        callback(components)
+      }
+    }
+  }
+
+  /**
+   * @param {EachCombinationFunc<T>} callback
+   */
+  eachCombination(callback) {
+    const components1 = new Array(this.descriptors.length)
+    const components2 = new Array(this.descriptors.length)
+
+    if (!this.components[0]) return
+
+    // This... many people will be very upset.
+    for (let j = 0; j < this.components[0].length; j++) {
+      for (let k = 0; k < this.components[0][j].length; k++) {
+        for (let l = j; l < this.components[0].length; l++) {
+          const nextup = l === j ? k + 1 : 0
+
+          for (let m = nextup; m < this.components[0][l].length; m++) {
+
+            for (let n = 0; n < this.descriptors.length; n++) {
+              components1[n] = this.components[n][j][k]
+              components2[n] = this.components[n][l][m]
+            }
+
+            // @ts-ignore
+            // SAFETY: Components are type cast to the expected passed value
+            callback(components1, components2)
+          }
+        }
+      }
+    }
+  }
+
+  /**
+   * @returns {T | null}
+   */
+  single() {
+    const components = new Array(this.descriptors.length)
+
+    if (!this.components[0] || !this.components[0][0] || !this.components[0][0][0]) return null
+
+    for (let i = 0; i < this.descriptors.length; i++) {
+      components[i] = this.components[i][0][0]
+    }
+
+    // @ts-ignore
+    // SAFETY: Components are type cast to the expected return value
+    return components
+  }
+
+  /**
+   * Returns the number of entities on this query.
+   *
+   * @returns {number}
+   */
+  count() {
+    let sum = 0
+
+    if (!this.components[0]) return 0
+
+    for (let i = 0; i < this.components[0].length; i++) {
+      sum += this.components[0][i].length
+    }
+
+    return sum
+  }
+}
