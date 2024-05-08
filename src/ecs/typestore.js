59196110bf0a0f69ca289f6a0de58b66960ba200
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2024-05-08 19:31:37 +0300

    [ecs] Added `TypeStore` which keeps record of types.

---
+++ src/ecs/typestore.js
@@ -0,0 +1,65 @@
+/** @import { ComponentId } from './typedef/index.js'*/
+import { ComponentInfo } from './component/index.js'
+
+export class TypeStore {
+
+  /**
+   * @type {Map<string,ComponentId>}
+   */
+  map = new Map()
+
+  /**
+   * @type {Array<ComponentInfo>}
+   */
+  list = []
+
+  /**
+   * @param {Function} componentClass
+   * @returns {ComponentId}
+   */
+  set(componentClass) {
+    const id = this.list.length
+    const name = componentClass.name.toLowerCase()
+
+    this.map.set(name, id)
+    this.list.push(new ComponentInfo(id, name))
+
+    return id
+  }
+
+  /**
+   * @param {string} name
+   * @returns {boolean}
+   */
+  has(name) {
+    return this.map.has(name)
+  }
+
+  /**
+   * @param {string} name
+   * @returns {ComponentInfo | undefined}
+   */
+  get(name) {
+    const id = this.getId(name)
+
+    if (id === void 0) return undefined
+
+    return this.getById(id)
+  }
+
+  /**
+   * @param {ComponentId} id
+   * @returns {ComponentInfo | undefined}
+   */
+  getById(id) {
+    return this.list[id]
+  }
+
+  /**
+   * @param {string} name
+   * @returns {ComponentId | undefined}
+   */
+  getId(name) {
+    return this.map.get(name)
+  }
+}
