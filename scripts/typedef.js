d2a96775f5d90089f1ede5ce8aa3648e58d7cc2f
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2024-05-23 13:27:34 +0300

    [script] Removed `removeTypeDefImport` and added measures to remove exports and ts no check tags.

--- scripts/typedef.js
+++ scripts/typedef.js
@@ -1,78 +1,56 @@
-import { readFileSync, writeFileSync, readdirSync } from "fs"
-import { join, resolve } from "path"
+import { readFileSync,writeFileSync,readdirSync } from "fs"
+import { join,resolve } from "path"
 
-//removeTypeDefImport("dist")
-concatTypedefs(resolve(process.cwd(), "src"), resolve(process.cwd(), "dist"))
+concatTypedefs(resolve(process.cwd(),"src"),resolve(process.cwd(),"dist"))
 
 /**
  * @param {string} input
  * @param {string} output
  */
-function concatTypedefs(input, output) {
-    const files = readdirSync(input, { recursive: true })
+function concatTypedefs(input,output) {
+    const files = readdirSync(input,{ recursive: true })
         .filter(str => str.includes("typedef"))
         .filter(path => path.includes(".js"))
+        .filter(path => !path.includes("index.js"))
     const outputfiles = readdirSync(output)
     let blob = ""
 
     for (const file of files) {
-        blob += readFileSync(join(input, file.toString()), { encoding: "utf-8" }) + "\n"
+        const fragment = readFileSync(join(input,file.toString()),{ encoding: "utf-8" })
+        const processedFragment = removeExport(removeNoCheck(fragment))
+
+        blob += "\n" + processedFragment
     }
 
     for (const filename of outputfiles) {
         if (filename.includes(".d.ts") || filename.includes(".min.")) continue
 
-        const acc = readFileSync(join(output, filename), { encoding: "utf-8" }) + blob
+        const acc = readFileSync(join(output,filename),{ encoding: "utf-8" }) + blob
 
-        writeFileSync(join(output, filename), acc)
+        writeFileSync(join(output,filename),acc)
     }
 }
 
 /**
- * @param {string} input
+ * Ensure the exports is in the bottom.
+ * @param {string} input 
+ * @returns {string}
  */
-function removeTypeDefImport(input) {
-    const paths = readdirSync(input)
-        .filter(path => !path.includes(".min."))
-        .filter(path => !path.includes(".d.ts"))
-        .filter(path => !path.includes(".umd."))
-        .map(path => resolve(input, path))
+function removeExport(input) {
+    const index = input.lastIndexOf("export")
 
-    for (let i = 0; i < paths.length; i++) {
-        const path = paths[i]
+    if (!input.includes('export')) return input
 
-        const output = cut(readFileSync(path).toString())
-
-        writeFileSync(path, output)
-    }
+    return input.slice(0,index)
 }
 
 /**
- * @param {string} input
+ * Ensure the no-check tag is there.
+ * @param {string} input 
+ * @returns {string}
  */
-function cut(input) {
-    const searchable = "@typedef"
-    let position = 0
-    let result = ""
+function removeNoCheck(input) {
+    if(!input.includes('@ts-nocheck')) return input
 
-    while (true) {
-        const location = input.indexOf(searchable, position)
-
-        if (location === -1) break
-
-        const start = input.indexOf("/**", position)
-        const end = input.indexOf("*/", location) + 2
-
-        if (input.slice(start, end).match("import")){
-            position = end
-            continue
-        }
-
-        result += input.slice(position, end)
-        position = end
-
-    }
-    result += input.slice(position)
-
-    return result
+    return input.slice(input.indexOf("\n") + 1,input.length)
 }
