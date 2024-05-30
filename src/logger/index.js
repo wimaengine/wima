0511b2f868a3490f9b085f7901622728823b27b7
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2024-05-30 16:53:24 +0300

    [logger] Added `warn` which logs out warnings to console.

---
+++ src/logger/index.js
@@ -0,0 +1,10 @@
+const marker = '🚀Chaos Engine:\n\n'
+
+/**
+ * Logs out a warning to the console.
+ *
+ * @param {string} message
+ */
+export function warn(message) {
+  console.warn(marker + message)
+}
