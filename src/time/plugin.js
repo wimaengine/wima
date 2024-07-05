925bbc55359105bfdc3f46cc29c97141f87bf85c
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2024-07-05 22:18:22 +0300

    [time] Added `TimePlugin`.

---
+++ src/time/plugin.js
@@ -0,0 +1,26 @@
+import { World } from '../ecs/index.js'
+import { VirtualClock } from './resource/index.js'
+import { App } from '../app/app.js'
+import { AppSchedule } from '../app/schedules.js'
+import { Clock } from './clock.js'
+
+export class TimePlugin {
+
+  /**
+   * @param {App} app
+   */
+  register(app) {
+    app
+    .setResource(new VirtualClock())
+    .registerSystem(AppSchedule.Update, updateVirtualClock)
+  }
+}
+
+/**
+ * @param {World} world
+ */
+function updateVirtualClock(world) {
+  const clock = world.getResource('virtualclock')
+
+  Clock.update(clock)
+}
