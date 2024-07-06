b3a31e7884c998d8d3f3d09981ab6b4aea77aa1f
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2024-07-06 12:02:22 +0300

    [profiler] Added `Profiler` which stores all preformance metrics.

--- src/profiler/resources/profiler.js
+++ src/profiler/resources/profiler.js
@@ -2,3 +2,51 @@ export class Profile {
   lastTick = 0
   delta = 0
 }
+
+export class Profiler {
+
+  /**
+   * @type {Map<string,Profile>}
+   */
+  profiles = new Map()
+
+  /**
+   * @param {string} label
+   */
+  set(label) {
+    const profile = new Profile()
+
+    this.profiles.set(label, profile)
+
+    return profile
+  }
+
+  /**
+   * @param {string} label
+   * @returns {Profile | undefined}
+   */
+  get(label) {
+    return this.profiles.get(label)
+  }
+
+  /**
+   * @param {string} label
+   */
+  start(label) {
+    const isthere = this.get(label)
+    const profile = isthere ? isthere : this.set(label)
+
+    profile.lastTick = performance.now()
+  }
+
+  /**
+   * @param {string} label
+   */
+  end(label) {
+    const profile = this.get(label)
+
+    if (!profile) return
+
+    profile.delta = performance.now() - profile.lastTick
+  }
+}
