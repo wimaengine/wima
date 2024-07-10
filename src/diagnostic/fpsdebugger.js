4f0069bbb4d885aae9408a08528c6c2b471dbe53
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2024-07-10 18:43:22 +0300

    [diagnostic] Added `FPSDebugger` to show framerate.

---
+++ src/diagnostic/fpsdebugger.js
@@ -0,0 +1,60 @@
+import { App, AppSchedule } from '../app/index.js'
+import { World } from '../ecs/index.js'
+import { Timer, TimerMode } from '../time/index.js'
+import { RAFTimer } from './resources/index.js'
+
+export class FPSDebugger {
+
+  /**
+   * @param {App} app
+   */
+  register(app) {
+    app
+      .setResource(new RAFTimer(1, TimerMode.Repeat))
+      .registerSystem(AppSchedule.Startup, setUpUI)
+      .registerSystem(AppSchedule.Update, updateFPSCounter)
+      .registerSystem(AppSchedule.Update, updateRAFTimer)
+  }
+}
+
+/**
+ *
+ */
+function setUpUI() {
+  const container = document.body.appendChild(document.createElement('div'))
+
+  container.id = 'fps-container'
+  container.style.position = 'absolute'
+  container.style.top = '0px'
+  container.style.right = '0px'
+  container.style.width = '100px'
+  container.style.height = '20px'
+  container.style.background = 'black'
+  container.style.textAlign = 'center'
+  container.style.color = 'white'
+}
+
+/**
+ * @param {World} world
+ */
+function updateFPSCounter(world) {
+  const clock = world.getResource('virtualclock')
+  const timer = world.getResource('raftimer')
+
+  if (!timer.finished) return
+
+  const container = document.querySelector('#fps-container')
+  const fps = Math.round(clock.fps)
+
+  if (container) container.innerHTML = `${fps} fps`
+}
+
+/**
+ * @param {World} world
+ */
+function updateRAFTimer(world) {
+  const clock = world.getResource('virtualclock')
+  const timer = world.getResource('raftimer')
+
+  Timer.update(timer, clock.delta)
+}
