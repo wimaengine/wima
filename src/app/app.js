71dca701a63c3e15f8e6decab56646af09b5f167
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2024-07-15 20:09:22 +0300

    [app] Added `App` which is the core of any application made.

---
+++ src/app/app.js
@@ -0,0 +1,132 @@
+/** @import { ChaosPlugin } from './typedef/index.js' */
+/** @import { SystemFunc } from '../ecs/index.js' */
+import { World, Scheduler, Executor, ComponentHooks, RAFExecutor, ImmediateExecutor } from '../ecs/index.js'
+import { assert } from '../logger/index.js'
+import { AppSchedule } from './schedules.js'
+
+const registererror = 'Systems, plugins or resources should be registered or set before `App().run()`'
+
+export class App {
+
+  /**
+   * @private
+   * @type {World}
+   */
+  world = new World()
+
+  /**
+   * @private
+   * @type {Scheduler}
+   */
+  scheduler = new Scheduler()
+
+  /**
+   * @private
+   * @type {boolean}
+   */
+  initialized = false
+
+  constructor() {
+    this.createSchedule(
+      AppSchedule.Startup,
+      new ImmediateExecutor()
+    )
+    this.createSchedule(
+      AppSchedule.Update,
+      new RAFExecutor()
+    )
+  }
+
+  /**
+   * Return the world of the app.
+   * 
+   * @returns {World}
+   */
+  getWorld(){
+    return this.world
+  }
+
+  /**
+   * @template {Executor} T
+   * @param {string} label
+   * @param {T} executor
+   */
+  createSchedule(label, executor) {
+    return this.scheduler.set(label, executor)
+  }
+
+  /**
+   * Starts up the {@link App}.
+   * Prevents calls to {@link App.registerSystem},
+   * {@link App.registerPlugin} and {@link App.setResource}.
+   * 
+   * @returns {this}
+   */
+  run() {
+    this.initialized = true
+    this.scheduler.run(this.world)
+
+    return this
+  }
+
+  /**
+   * @param {ChaosPlugin} plugin
+   */
+  registerPlugin(plugin) {
+    plugin.register(this)
+
+    return this
+  }
+
+  /**
+   * @param {ChaosPlugin} debug
+   */
+  registerDebugger(debug) {
+    return this.registerPlugin(debug)
+  }
+
+  /**
+   * @param {string} label
+   * @param {SystemFunc} system
+   */
+  registerSystem(label, system) {
+    const schedule = this.scheduler.get(label)
+
+    assert(!this.initialized, registererror)
+    assert(schedule, `The system ${system.name} cannot be added to schedule "${label}" as the schedule doesn't exist.`)
+
+    schedule.add(system)
+
+    return this
+  }
+
+  /**
+   * @param {Function} type
+   */
+  registerType(type) {
+    this.world.registerType(type)
+
+    return this
+  }
+
+  /**
+   * @param {Function} component
+   * @param {ComponentHooks} hooks
+   */
+  setComponentHooks(component, hooks) {
+    this.world.setComponentHooks(component.name.toLowerCase(), hooks)
+
+    return this
+  }
+
+  /**
+   * @template {object} T
+   * @param {T} resource
+   */
+  setResource(resource) {
+    assert(!this.initialized, registererror)
+    this.world.setResource(resource)
+
+    return this
+  }
+}
