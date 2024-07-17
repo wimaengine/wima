14959a4e8f196df2ef4fb7e30d26df6fb7dfb9bd
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2024-07-17 17:24:00 +0300

    [movable] Added `createMovable2D()` to create movable entities.

---
+++ src/movable/prefabs/movable2d.js
@@ -0,0 +1,12 @@
+import { Orientation2D, Position2D, Scale2D } from '../../transform/index.js'
+import { Velocity2D, Rotation2D, Acceleration2D, Torque2D } from '../components/index.js'
+
+/**
+ * @param {number | undefined} x
+ * @param {number | undefined} y
+ * @param {number | undefined} a
+ * @returns {[Position2D, Orientation2D, Scale2D, Velocity2D, Rotation2D, Acceleration2D, Torque2D]}
+ */
+export function createMovable2D(x, y, a) {
+  return [new Position2D(x, y), new Orientation2D(a), new Scale2D(), new Velocity2D(), new Rotation2D(), new Acceleration2D(), new Torque2D()]
+}
