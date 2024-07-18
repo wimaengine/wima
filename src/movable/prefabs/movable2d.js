80f3ae5f063bec6ab6df54958ed7b94e5d054eba
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2024-07-18 10:51:00 +0300

    [movable] Added `createRawMovable2D()` to create movable entities without transform.

--- src/movable/prefabs/movable2d.js
+++ src/movable/prefabs/movable2d.js
@@ -10,3 +10,13 @@ import { Velocity2D, Rotation2D, Acceleration2D, Torque2D } from '../components/
 export function createMovable2D(x, y, a) {
   return [new Position2D(x, y), new Orientation2D(a), new Scale2D(), new Velocity2D(), new Rotation2D(), new Acceleration2D(), new Torque2D()]
 }
+
+/**
+ * @param {number} x
+ * @param {number} y
+ * @param {number} a
+ * @returns {[Velocity2D,Rotation2D,Acceleration2D,Torque2D]}
+ */
+export function createRawMovable2D(x = 0, y = 0, a = 0) {
+  return [new Velocity2D(x, y), new Rotation2D(a), new Acceleration2D(), new Torque2D()]
+}
