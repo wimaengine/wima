2f9fbfacef0a1d400e3409d9d0ec2276b6672b06
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2024-08-01 10:51:00 +0300

    [gravity] Added `applyGravity3D()` system.

--- src/gravity/systems/index.js
+++ src/gravity/systems/index.js
@@ -1,5 +1,5 @@
 import { Query, World } from '../../ecs/index.js'
-import { Vector2 } from '../../math/index.js'
+import { Vector2, Vector3 } from '../../math/index.js'
 
 /**
  * @param {World} world
@@ -12,6 +12,24 @@ export function applyGravity2D(world) {
     if (properties.invmass === 0) return
 
     Vector2.add(
+      acceleration,
+      gravity,
+      acceleration
+    )
+  })
+}
+
+/**
+ * @param {World} world
+ */
+export function applyGravity3D(world) {
+  const gravity = world.getResource('gravity3d')
+  const query = new Query(world, ['acceleration3d', 'physicsproperties'])
+
+  query.each(([acceleration, properties]) => {
+    if (properties.invmass === 0) return
+
+    Vector3.add(
       acceleration,
       gravity,
       acceleration
