7c9ae638fdfd073177726370f880bc89ff8fc783
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2024-08-10 18:58:40 +0300

    [logger] Added `warnOnce` which warns a message only once.

--- src/logger/index.js
+++ src/logger/index.js
@@ -1,4 +1,5 @@
 const marker = '🚀Chaos Engine:\n\n'
+const mess = ['']
 
 /**
  * Logs out a warning to the console.
@@ -7,4 +8,16 @@ const marker = '🚀Chaos Engine:\n\n'
  */
 export function warn(message) {
   console.warn(marker + message)
+}
+
+/**
+ * Logs out a warning once to the console.
+ *
+ * @param {string} message
+ */
+export function warnOnce(message) {
+  if (mess.includes(message)) return
+
+  mess.push(message)
+  warn(message)
 }
