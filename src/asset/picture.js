cf93e16775d0b91d475641d0515079bbc15b5f7b
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2024-08-12 21:14:41 +0300

    [asset] Added `Picture`.

---
+++ src/asset/picture.js
@@ -0,0 +1,28 @@
+import { Vector2 } from '../math/index.js'
+
+// This is supposed to be `Image()` but that is already taken.
+export class Picture {
+
+  /**
+   * @type {ArrayBuffer}
+   */
+  raw
+
+  /**
+   * @type {Vector2}
+   */
+  dimensions
+
+  /**
+   * @param {ArrayBuffer} buffer
+   * @param {Vector2} dimensions
+   */
+  constructor(buffer, dimensions) {
+    this.raw = buffer
+    this.dimensions = dimensions
+  }
+  static PLACEHOLDER = new Picture(
+    new ArrayBuffer(0),
+    new Vector2()
+  )
+}
