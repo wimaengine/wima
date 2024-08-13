df5027d52f8c05c1e2a3bf558b73ba248f892709
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2024-08-13 12:41:22 +0300

    [device] Added `Browser`.

---
+++ src/device/core/browser.js
@@ -0,0 +1,13 @@
+/**
+ * @readonly
+ * @enum {number}
+ */
+export const Browser = {
+    Unknown: 0,
+    Chrome: 1,
+    FireFox: 2,
+    Safari: 3,
+    internetExplorer: 4,
+    Opera: 5,
+    Edge: 6
+}
