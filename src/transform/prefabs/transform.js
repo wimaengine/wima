073ec575b19b04576755f92f28f50dffedca8e11
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2024-08-16 20:28:22 +0300

    [transform] Added `createTransform3D`.

---
+++ src/transform/prefabs/transform.js
@@ -0,0 +1,23 @@
+import { Position2D, Orientation2D, Scale2D } from '../components/index.js'
+
+/**
+ * @param { number } dx
+ * @param { number } dy
+ * @param { number } a
+ * @param { number } sx
+ * @param { number } sy
+ * @returns {[Position2D,Orientation2D,Scale2D]}
+ */
+export function createTransform2D(
+  dx = 0,
+  dy = 0,
+  a = 0,
+  sx = 1,
+  sy = 1
+) {
+  return [
+    new Position2D(dx, dy),
+    new Orientation2D(a),
+    new Scale2D(sx, sy)
+    ]
+}
