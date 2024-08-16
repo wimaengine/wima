4d6d8120e74cbae729ccabab5f33cb51437d0f3a
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2024-08-16 22:00:22 +0300

    [transform] Added `createTransform3D`.

---
+++ src/transform/prefabs/transform3d.js
@@ -0,0 +1,31 @@
+import { Position3D, Orientation3D, Scale3D } from '../components/index.js'
+
+/**
+ * @param { number } x
+ * @param { number } y
+ * @param { number } z
+ * @param {number} [ox=0]
+ * @param {number} [oy=0]
+ * @param {number} [oz=0]
+ * @param { number } sx
+ * @param { number } sy
+ * @param { number } sz
+ * @returns {[Position3D,Orientation3D,Scale3D]}
+ */
+export function createTransform3D(
+  x = 0,
+  y = 0,
+  z = 0,
+  ox = 0,
+  oy = 0,
+  oz = 0,
+  sx = 1,
+  sy = 1,
+  sz = 1
+) {
+  return [
+    new Position3D(x, y, z),
+    new Orientation3D().fromEuler(ox, oy, oz),
+    new Scale3D(sx, sy, sz)
+    ]
+}
