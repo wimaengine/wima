7925d969f4b7321d0991bb3d35524da2cfb3e6f4
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2024-08-22 22:34:22 +0300

    [math] Added `Angle` for 2d angles.

---
+++ src/math/angle.js
@@ -0,0 +1,29 @@
+/**
+ * Wrapper class since JavaScript doesn't support references to numbers explicitly.
+ * Keeps record of the orientation of an entity.
+ */
+export class Angle {
+
+  /**
+   * Orientation in radians.
+   *
+   * @type {number}
+   */
+  value = 0
+
+  /**
+   * @param {number} [rad=0] - Angle in radians.
+   */
+  constructor(rad = 0) {
+    this.value = rad
+  }
+
+  /**
+   * Copies the orientation of another angle.
+   *
+   * @param {Angle} angle
+   */
+  static copy(angle) {
+    this.value = angle.value
+  }
+}
