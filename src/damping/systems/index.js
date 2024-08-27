1f2bafd6e044a40d5bafc5f270614a583fb53055
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2024-08-27 22:36:00 +0300

    [damping] Added `dampenVelocity3D()` system.

--- src/damping/systems/index.js
+++ src/damping/systems/index.js
@@ -1,6 +1,6 @@
 import { Query, World } from '../../ecs/index.js'
-import { Vector2 } from '../../math/index.js'
-import { Velocity2D } from '../../movable/index.js'
+import { Vector2, Vector3 } from '../../math/index.js'
+import { Velocity2D, Velocity3D } from '../../movable/index.js'
 
 /**
  * @param {World} world
@@ -14,5 +14,20 @@ export function dampenVelocity2D(world) {
   
   query.each(([velocity]) => {
     Vector2.multiplyScalar(velocity, linear, velocity)
+  })
+}
+
+/**
+ * @param {World} world
+ */
+export function dampenVelocity3D(world) {
+
+  /** @type {Query<[Velocity3D]>} */
+  const query = new Query(world, ['velocity3d'])
+
+  const linear = 1 - world.getResource('lineardamping')
+  
+  query.each(([velocity]) => {
+    Vector3.multiplyScalar(velocity, linear, velocity)
   })
 }
