f0b2ca7276d0cc445a7d1a5012580a516e429a84
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2024-09-07 16:25:22 +0300

    [event] Added `CEvent` to repesent an event.

---
+++ src/event/core/event.js
@@ -0,0 +1,20 @@
+/**
+ * Represents an event.
+ * 
+ * @template T
+ */
+export class CEvent {
+
+    /**
+     * @readonly
+     * @type {T}
+     */
+    data
+    
+    /**
+     * @param {T} data
+     */
+    constructor(data) {
+      this.data = data
+    }
+  }
