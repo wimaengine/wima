9a8a694bf1a693438bf3224922a2a8077ae66d4b
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2024-09-10 20:08:22 +0300

    [datastructures] Added `IndexAllocator` for reserving indices.

---
+++ src/datastructures/indexallocator.js
@@ -0,0 +1,36 @@
+export class IndexAllocator {
+
+    /**
+     * @private
+     * @type {number}
+     */
+    nextid = 0
+
+    /**
+     * @private
+     * @type {number[]}
+     */
+    recycled = []
+
+    /**
+     * @param {number} index 
+     */
+    recycle(index){
+        this.recycled.push(index)
+    }
+
+    /**
+     * @returns {number}
+     */
+    reserve(){
+        const recycled = this.recycled.pop()
+        
+        if(recycled !== undefined) return recycled
+
+        const index = this.nextid
+
+        this.nextid += 1
+
+        return index
+    }
+}
