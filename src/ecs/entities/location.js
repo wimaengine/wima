eff4d5802744117b1de3ef0ad9640e784dafb8d4
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2024-09-11 13:27:22 +0300

    [ecs] Added `EntityLocation`.

---
+++ src/ecs/entities/location.js
@@ -0,0 +1,13 @@
+/** @import {ArchetypeId} from '../typedef/index.js' */
+
+export class EntityLocation {
+
+    /**
+     * @param {ArchetypeId} archid
+     * @param {number} index 
+     */
+    constructor(archid, index) {
+        this.archid = archid
+        this.index = index
+    }
+}
