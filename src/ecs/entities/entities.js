3fb8709b440487cb3ef144d4a41727145dcf324a
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2024-09-11 13:27:22 +0300

    [ecs] Added `Entities` which binds entities with a location in storage.

---
+++ src/ecs/entities/entities.js
@@ -0,0 +1,9 @@
+import { DenseList } from '../../datastructures/index.js'
+import { EntityLocation } from './location.js'
+
+/** @augments {DenseList<EntityLocation>} */
+export class Entities extends DenseList {
+    reserve(){
+        return this.push(new EntityLocation(-1, -1))
+    }
+}
