533225795bd5b5203ec92cfe9c33ea5f48630bde
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2024-10-01 02:11:58 +0300

    [datastructures] Applied linting rules #ignore

--- src/datastructures/denselist.js
+++ src/datastructures/denselist.js
@@ -6,58 +6,58 @@ import { IndexAllocator } from './indexallocator.js'
  */
 export class DenseList {
 
-    /**
-     * @private
-     * @type {T[]}
-     */
-    list = []
+  /**
+   * @private
+   * @type {T[]}
+   */
+  list = []
 
-    /**
-     * @private
-     * @type {IndexAllocator}
-     */
-    allocator = new IndexAllocator()
+  /**
+   * @private
+   * @type {IndexAllocator}
+   */
+  allocator = new IndexAllocator()
 
-    /**
-     * @param {T} object 
-     * @returns {number}
-     */
-    push(object){
-        const index = this.allocator.reserve()
+  /**
+   * @param {T} object 
+   * @returns {number}
+   */
+  push(object){
+    const index = this.allocator.reserve()
 
-        this.list[index] = object
+    this.list[index] = object
 
-        return index
-    }
+    return index
+  }
 
-    /**
-     * @param {number} index
-     */
-    recycle(index){
-        this.allocator.recycle(index)
-    }
+  /**
+   * @param {number} index
+   */
+  recycle(index){
+    this.allocator.recycle(index)
+  }
     
-    /**
-     * @param {number} index 
-     * @returns {T | undefined}
-     */
-    get(index){
-        return this.list[index]
-    }
+  /**
+   * @param {number} index 
+   * @returns {T | undefined}
+   */
+  get(index){
+    return this.list[index]
+  }
 
-    /**
-     * @param {number} index 
-     * @param {T} object 
-     */
-    set(index, object){
-        assert(index <= this.allocator.count(), 'The index provided has never been allocated' )
-        this.list[index] = object    
-    }
+  /**
+   * @param {number} index 
+   * @param {T} object 
+   */
+  set(index, object){
+    assert(index <= this.allocator.count(), 'The index provided has never been allocated' )
+    this.list[index] = object    
+  }
 
-    /**
-     * @returns {number}
-     */
-    reserve(){
-        return this.allocator.reserve()
-    }
+  /**
+   * @returns {number}
+   */
+  reserve(){
+    return this.allocator.reserve()
+  }
 }
--- src/datastructures/indexallocator.js
+++ src/datastructures/indexallocator.js
@@ -1,39 +1,39 @@
 export class IndexAllocator {
 
-    /**
-     * @private
-     * @type {number}
-     */
-    nextid = 0
+  /**
+   * @private
+   * @type {number}
+   */
+  nextid = 0
 
-    /**
-     * @private
-     * @type {number[]}
-     */
-    recycled = []
+  /**
+   * @private
+   * @type {number[]}
+   */
+  recycled = []
 
-    /**
-     * @param {number} index 
-     */
-    recycle(index){
-        this.recycled.push(index)
-    }
+  /**
+   * @param {number} index 
+   */
+  recycle(index){
+    this.recycled.push(index)
+  }
 
-    /**
-     * @returns {number}
-     */
-    reserve(){
-        const recycled = this.recycled.pop()
+  /**
+   * @returns {number}
+   */
+  reserve(){
+    const recycled = this.recycled.pop()
         
-        if(recycled !== undefined) return recycled
+    if(recycled !== undefined) return recycled
 
-        const index = this.nextid
+    const index = this.nextid
 
-        this.nextid += 1
+    this.nextid += 1
 
-        return index
-    }
-    count(){
-        return (this.nextid - 1)
-    }
+    return index
+  }
+  count(){
+    return (this.nextid - 1)
+  }
 }
