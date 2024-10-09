fe568c70ba43165b53eeb1195392349c99ede773
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2024-10-09 21:50:00 +0300

    [audio] Added `AudioPlugin` to register audio related functionality.

---
+++ src/audio/plugin.js
@@ -0,0 +1,30 @@
+import { App } from '../app/index.js'
+import { Audio } from '../asset/index.js'
+import { AudioCommands, AudioParser } from './resources/index.js'
+
+export class AudioPlugin {
+
+  /**
+   * @param {App} app
+   */
+  register(app) {
+    const handler = new AudioCommands()
+
+    app
+      .setResource(handler)
+      .registerAsset(Audio)
+      .registerAssetParser(Audio, new AudioParser())
+    window.addEventListener('pointerdown', resumeAudio)
+
+    /** */
+    function resumeAudio() {
+      const ctx = handler.getContext()
+
+      ctx.resume()
+
+      if (ctx.state === 'running') {
+        removeEventListener('pointerdown', resumeAudio)
+      }
+    }
+  }
+}
