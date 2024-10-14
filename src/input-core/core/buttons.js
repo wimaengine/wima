79ba069afceb60bdf0d5f8e4e441154aa751c6d8
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2024-10-14 21:18:00 +0300

    [input-core] Added `Buttons` to represent the state of a set of buttons.

---
+++ src/input-core/core/buttons.js
@@ -0,0 +1,72 @@
+/**
+ * @template T
+ */
+export class Buttons {
+
+  /**
+   * @private
+   * @type {Set<T>}
+   */
+  innerPressed = new Set()
+
+  /**
+   * @private
+   * @type {Set<T>}
+   */
+  innerJustPressed = new Set()
+
+  /**
+   * @private
+   * @type {Set<T>}
+   */
+  innerJustReleased = new Set()
+
+  /**
+   * @param {T} button
+   */
+  press(button) { 
+    this.innerPressed.add(button)
+    this.innerJustPressed.add(button)
+  }
+
+  /**
+   * @param {T} button
+   */
+  release(button) { 
+    this.innerJustReleased.add(button)
+    this.innerPressed.delete(button)
+  }
+
+  /**
+   * @param {T} button 
+   * @returns {boolean}
+   */
+  pressed(button) {
+    return this.innerPressed.has(button)
+  }
+
+  /**
+   * @param {T} button 
+   * @returns {boolean}
+   */
+  justPressed(button) {
+    return this.innerPressed.has(button)
+  }
+
+  /**
+   * @param {T} button 
+   * @returns {boolean}
+   */
+  justReleased(button) {
+    return this.innerJustReleased.has(button)
+  }
+  clearJustPressed() {
+    this.innerJustPressed.clear()
+  }
+  clearJustReleased() {
+    this.innerJustReleased.clear()
+  }
+  clearPressed() {
+    this.innerPressed.clear()
+  }
+}
