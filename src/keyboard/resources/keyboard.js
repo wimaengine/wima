ee988535400f0e61bde5888fd9f608fdd2c21836
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2024-10-16 21:34:00 +0300

    [keyboard] Added `Keyboard` representing a US physical keyboard.

---
+++ src/keyboard/resources/keyboard.js
@@ -0,0 +1,9 @@
+import { KeyCode } from '../core/index.js'
+import { Buttons } from '../../input-core/index.js'
+
+/**
+ * Handles the keyboard input of an application on a PC.
+ * 
+ * @augments {Buttons<KeyCode>}
+ */
+export class Keyboard extends Buttons {}
