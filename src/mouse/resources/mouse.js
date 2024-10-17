15c1ab523fcb17d5b88b8390dd89e01ec6a8e4b8
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2024-10-17 13:46:00 +0300

    [mouse] Added `Mouse` to represent physical mouse position.

---
+++ src/mouse/resources/mouse.js
@@ -0,0 +1,22 @@
+import { Vector2 } from '../../math/index.js'
+
+/**
+ * This handles all inputs from mouse and touchpad on PC and laptop.
+ */
+export class Mouse {
+
+  /**
+   * Distance vector between the last frame's position and current position.
+   * 
+   * @type {Vector2}
+   */
+  lastPosition = new Vector2()
+  
+  /**
+   * Position of the mouse in current frame.
+   * 
+   * @type {Vector2}
+   */
+  position = new Vector2()
+}
+  
