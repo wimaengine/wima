9563c6122932fc740ebfaeca5d9f1900d9e5de1c
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2024-10-17 13:48:00 +0300

    [mouse] Added `MouseButtons` to store state of mouse buttons.

---
+++ src/mouse/resources/mousebutton.js
@@ -0,0 +1,7 @@
+import { MouseButton } from '../core/index.js'
+import { Buttons } from '../../input-core/index.js'
+
+/**
+ * @augments {Buttons<MouseButton>}
+ */
+export class MouseButtons extends Buttons { }
