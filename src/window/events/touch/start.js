e4740809aa09a259946d98ce95aead5f7943867f
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2024-10-21 22:22:00 +0300

    [window] Added `TouchStart` as event payload for touch start.

---
+++ src/window/events/touch/start.js
@@ -0,0 +1,24 @@
+import { Vector2 } from '../../../math/index.js'
+
+export class TouchStart {
+
+  /**
+   * @readonly
+   * @type {number}
+   */
+  id
+
+  /**
+   * @readonly
+   * @type {Vector2}
+   */
+  position = new Vector2()
+
+  /**
+   * @param {Touch} event
+   */
+  constructor(event) {
+    this.id = event.identifier
+    this.position.set(event.clientX, event.clientY)
+  }
+}
