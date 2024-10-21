f9e8ef28f3b7c7c7fb9f29666902ee895fa415e2
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2024-10-22 00:14:00 +0300

    [window-dom] Added `setUpFileEvents` to set file events on dom window,

---
+++ src/window-dom/core/file.js
@@ -0,0 +1,21 @@
+import { World } from '../../ecs/index.js'
+import { FileDrop, FileDrag } from '../../window/index.js'
+
+/**
+ * @param {World} world
+ * @param {HTMLElement } target
+ */
+export function setUpFileEvents(world, target) {
+  target.addEventListener('dragover', (event) => {
+    const dispatch = world.getResource('events<filedrag>')
+
+    dispatch.write(new FileDrag(event))
+    event.preventDefault()
+  })
+  target.addEventListener('drop', (event) => {
+    const dispatch = world.getResource('events<filedrop>')
+
+    dispatch.write(new FileDrop(event))
+    event.preventDefault()
+  })
+}
