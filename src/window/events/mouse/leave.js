fd6eb38620764ac7d765a27c9f577d98bf32d97c
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2024-10-21 22:06:00 +0300

    [window] Added `MouseLeave` as event payload for mouse leaving an area.

---
+++ src/window/events/mouse/leave.js
@@ -0,0 +1,17 @@
+import { Vector2 } from '../../../math/index.js'
+
+export class MouseLeave {
+  
+  /**
+   * @readonly
+   * @type {Vector2}
+   */
+  position = new Vector2()
+  
+  /**
+   * @param {MouseEvent} event
+   */
+  constructor(event) {
+    this.position.set(event.x, event.y)
+  }
+}
