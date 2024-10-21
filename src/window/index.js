1acce76d99d2b0d9cd4ecd6fd90bc12331bcf630
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2024-10-22 00:12:00 +0300

    [window] Added centralised exports. #internal

---
+++ src/window/index.js
@@ -0,0 +1,7 @@
+export * from './commands/index.js'
+export * from './components/index.js'
+export * from './core/index.js'
+export * from './events/index.js'
+export * from './resources/index.js'
+export * from './prefabs/index.js'
+export * from './plugin.js'
