612c92e00f142adb9bc4ab3afd9248cdee61ccc4
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2024-10-21 19:51:00 +0300

    [window] Added `WindowRequest` for window related actions.

---
+++ src/window/core/request.js
@@ -0,0 +1,12 @@
+/**
+ * @readonly
+ * @enum {number}
+ */
+export const WindowRequest = {
+  FullScreen:0,
+  PointerLock:1,
+  PointerCapture:2,
+  PointerRelease:3,
+  Resize:4,
+  Reposition:5
+}
