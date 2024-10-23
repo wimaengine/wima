56b392f9247de84fa3eb89d5e60dcd09de728212
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2024-10-23 18:46:00 +0300

    [window-dom] Added `setUpWindowEvents()` to set window events on dom window,

---
+++ src/window-dom/core/window.js
@@ -0,0 +1,14 @@
+import { World } from '../../ecs/index.js'
+import { WindowResize } from '../../window/index.js'
+
+/**
+ * @param {World} world
+ * @param {HTMLElement } target
+ */
+export function setUpWindowEvents(world, target) {
+  target.addEventListener('resize', (event) => {
+    const dispatch = world.getResource('events<windowresize>')
+
+    dispatch.write(new WindowResize(event))
+  }) 
+}
