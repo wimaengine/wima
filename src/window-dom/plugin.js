9b718a0d7274f36179f0fc1dcf1a81e7bd707edf
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2024-10-23 19:00:00 +0300

    [window-dom] Added `DOMWindowPlugin` for dom windowing dunctionality.

---
+++ src/window-dom/plugin.js
@@ -0,0 +1,17 @@
+import { App, AppSchedule } from '../app/index.js'
+import { ComponentHooks } from '../ecs/index.js'
+import { Window } from '../window/index.js'
+import { closeWindow, openWindow } from './hooks/index.js'
+import { executeWindowCommands } from './systems/index.js'
+
+export class DOMWindowPlugin {
+
+  /**
+   * @param {App} app
+   */
+  register(app) {
+    app
+      .setComponentHooks(Window, new ComponentHooks(openWindow, closeWindow))
+      .registerSystem(AppSchedule.Update, executeWindowCommands)
+  }
+}
