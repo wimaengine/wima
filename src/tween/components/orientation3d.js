53b35b66af57265bdd922356488b73879ba33361
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2024-10-25 20:04:00 +0300

    [tween] Added `Orientation3DTween` to tween 3d orientations.

---
+++ src/tween/components/orientation3d.js
@@ -0,0 +1,7 @@
+import { Orientation3D } from '../../transform/index.js'
+import { Tween } from './tween.js'
+
+/**
+ * @augments {Tween<Orientation3D>}
+ */
+export class Orientation3DTween extends Tween {}
