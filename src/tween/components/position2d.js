467f246dce584d20ac32d6325d381cb1836da223
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2024-10-25 20:03:00 +0300

    [tween] Added `Position2DTween` to tween 2d positions.

---
+++ src/tween/components/position2d.js
@@ -0,0 +1,7 @@
+import { Position2D } from '../../transform/index.js'
+import { Tween } from './tween.js'
+
+/**
+ * @augments {Tween<Position2D>}
+ */
+export class Position2DTween extends Tween {}
