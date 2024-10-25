24fa63b368d7c0ff6f928bca1df7d481a82fc866
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2024-10-25 20:04:00 +0300

    [tween] Added `Orientation2DTween` to tween 2d orientations.

---
+++ src/tween/components/orientation2d.js
@@ -0,0 +1,7 @@
+import { Orientation2D } from '../../transform/index.js'
+import { Tween } from './tween.js'
+
+/**
+ * @augments {Tween<Orientation2D>}
+ */
+export class Orientation2DTween extends Tween {}
