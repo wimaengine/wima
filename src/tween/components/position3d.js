c025029be74bd37a9e7499e54f6f8fae09d79b26
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2024-10-25 20:04:00 +0300

    [tween] Added `Position3DTween` to tween 3d positions.

---
+++ src/tween/components/position3d.js
@@ -0,0 +1,7 @@
+import { Position3D } from '../../transform/index.js'
+import { Tween } from './tween.js'
+
+/**
+ * @augments {Tween<Position3D>}
+ */
+export class Position3DTween extends Tween {}
