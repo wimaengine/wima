06489d5aede90eb38c2bc2e077af9034e0a30d0e
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2024-10-26 19:40:00 +0300

    [integrator] Added `EulerIntegrator3DPlugin`.

--- src/integrator/plugins/euler.js
+++ src/integrator/plugins/euler.js
@@ -3,7 +3,11 @@ import {
   updateAngularEuler2D,
   updateOrientationEuler2D,
   updatePositionEuler2D,
-  updateVelocityEuler2D
+  updateVelocityEuler2D,
+  updateAngularEuler3D,
+  updateOrientationEuler3D,
+  updatePositionEuler3D,
+  updateVelocityEuler3D
 } from '../systems/index.js'
 
 export class EulerIntegrator2DPlugin {
@@ -17,5 +21,19 @@ export class EulerIntegrator2DPlugin {
       .registerSystem(AppSchedule.Update, updateVelocityEuler2D)
       .registerSystem(AppSchedule.Update, updateOrientationEuler2D)
       .registerSystem(AppSchedule.Update, updateAngularEuler2D)
+  }
+}
+
+export class EulerIntegrator3DPlugin {
+
+  /**
+   * @param {App} app
+   */
+  register(app) {
+    app
+      .registerSystem(AppSchedule.Update, updatePositionEuler3D)
+      .registerSystem(AppSchedule.Update, updateVelocityEuler3D)
+      .registerSystem(AppSchedule.Update, updateOrientationEuler3D)
+      .registerSystem(AppSchedule.Update, updateAngularEuler3D)
   }
 }
