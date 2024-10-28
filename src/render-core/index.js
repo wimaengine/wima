5149a7b1bc871621f63a7161e0d1c02266ba4125
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2024-10-28 17:38:00 +0300

    [render-core] Added centralised exports. #internal

---
+++ src/render-core/index.js
@@ -0,0 +1,6 @@
+export * from './assets/index.js'
+export * from './components/index.js'
+export * from './resources/index.js'
+export * from './prefabs/index.js'
+export * from './core/index.js'
+export * from './plugin.js'
