0040abb9a654a25223e379139731f8d17d89bcc0
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2024-10-28 17:32:00 +0300

    [render-core] Added `createCamera2D()` for 2d camera creation.

---
+++ src/render-core/prefabs/camera2d.js
@@ -0,0 +1,14 @@
+import { Orientation2D, Position2D, Scale2D } from '../../transform/index.js'
+import { Camera } from '../components/index.js'
+
+/**
+ * @param {number} x
+ * @param {number} y
+ * @param {number} a
+ * @param {number} sx
+ * @param {number} sy
+ * @returns {[Position2D,Orientation2D,Scale2D,Camera]}
+ */
+export function createCamera2D(x = 0, y = 0, a = 0, sx = 1, sy = 1) {
+  return [new Position2D(x, y), new Orientation2D(a), new Scale2D(sx, sy), new Camera()]
+}
