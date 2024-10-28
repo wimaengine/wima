255f61f43fb4156b227ee494fa6c3a97356ee9b9
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2024-10-28 17:23:00 +0300

    [render-core] Added `Material` asset.

---
+++ src/render-core/assets/material.js
@@ -0,0 +1,22 @@
+
+export class Material {
+
+  /**
+   * @returns {string}
+   */
+  vertex(){
+    return ''
+  }
+
+  /**
+   * @returns {string}
+   */
+  fragment(){
+    return ''
+  }
+  static default(){
+    return new Material()
+  }
+}
+
+export default {}
