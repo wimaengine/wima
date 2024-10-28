0aea8ccacb6c4caaf8e657463f66586425d6eea0
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2024-10-28 17:31:00 +0300

    [render-core] Added `MaterialHandle` for ref to a rendering material.

---
+++ src/render-core/components/material.js
@@ -0,0 +1,8 @@
+/** @import { MaterialId } from '../typedef/index.js' */
+import { Material } from '../assets/index.js'
+import { Handle } from '../../asset/index.js'
+
+/**
+ * @augments {Handle<Material>}
+ */
+export class MaterialHandle extends Handle {}
