dd67ad0d9fb59eccbaa1cb72553c19127cf13f45
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2024-11-03 17:04:00 +0300

    [render] Added `Mesh.indices` property.

--- src/render-core/assets/mesh.js
+++ src/render-core/assets/mesh.js
@@ -6,6 +6,12 @@ export class Mesh {
 
   /**
    * @private
+   * @type {Uint16Array | undefined}
+   */
+  indices
+
+  /**
+   * @private
    * @type {Map<string,Attribute>}
    */
   attributes = new Map()
