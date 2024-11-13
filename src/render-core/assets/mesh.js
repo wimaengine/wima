3ec407c67cbb8c9884d3e2a82632db87923054ca
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2024-11-13 12:46:00 +0300

    [render] Added `Mesh.torus()` for creating toruses.

--- src/render-core/assets/mesh.js
+++ src/render-core/assets/mesh.js
@@ -635,6 +635,63 @@ export class Mesh {
     return mesh
 
   }
+
+  static torus(radius = 0.375, tube = 0.125, radialSegments = 12, tubularSegments = 48, arc = Math.PI * 2) {
+    const mesh = new Mesh()
+    const indices = []
+    const vertices = []
+    const normals = []
+    const uvs = []
+
+    const center = new Vector3()
+    const vertex = new Vector3()
+    const normal = new Vector3()
+
+    for (let j = 0; j <= radialSegments; j++) {
+      for (let i = 0; i <= tubularSegments; i++) {
+        const u = i / tubularSegments * arc
+        const v = j / radialSegments * Math.PI * 2
+
+        vertex.x = (radius + tube * Math.cos(v)) * Math.cos(u)
+        vertex.y = (radius + tube * Math.cos(v)) * Math.sin(u)
+        vertex.z = tube * Math.sin(v)
+
+        vertices.push(vertex.x, vertex.y, vertex.z)
+
+        center.x = radius * Math.cos(u)
+        center.y = radius * Math.sin(u)
+
+        Vector3.sub(vertex, center, normal).normalize()
+
+        normals.push(normal.x, normal.y, normal.z)
+
+        uvs.push(i / tubularSegments)
+        uvs.push(j / radialSegments)
+      }
+    }
+
+    for (let j = 1; j <= radialSegments; j++) {
+
+      for (let i = 1; i <= tubularSegments; i++) {
+        const a = (tubularSegments + 1) * j + i - 1
+        const b = (tubularSegments + 1) * (j - 1) + i - 1
+        const c = (tubularSegments + 1) * (j - 1) + i
+        const d = (tubularSegments + 1) * j + i
+
+        indices.push(a, b, d)
+        indices.push(b, c, d)
+      }
+    }
+
+    mesh
+      .setIndices(new Uint16Array(indices))
+      .setAttribute('position3d', new Attribute(new Float32Array(vertices)))
+      .setAttribute('normal3d', new Attribute(new Float32Array(normals)))
+      .setAttribute('uv', new Attribute(new Float32Array(uvs)))
+
+    return mesh
+
+  }
   
   static default() {
     return new Mesh()
