3d70f7e74e7a5e2f7f9b10e75cc96a41fed502c1
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2024-11-13 12:44:00 +0300

    [render] Added `Mesh.circle3D()` for creating flat 3d circles.

--- src/render-core/assets/mesh.js
+++ src/render-core/assets/mesh.js
@@ -152,6 +152,52 @@ export class Mesh {
 
     return mesh
   }
+
+  static circle3D(radius = 0.5, segments = 32, arcstart = 0, arclength = Math.PI * 2) {
+    const mesh = new Mesh()
+    const vertices = [0, 0, 0]
+    const normals = [0, 0, 1]
+    const uvs = [0.5, 0.5]
+    const indices = []
+    const angleIncrement = arclength / segments
+    const epilson = Math.pow(2, -31)
+
+    for (let i = arcstart; i < arclength + epilson; i += angleIncrement) {
+      const cos = Math.cos(i)
+      const sin = Math.sin(i)
+
+      vertices.push(
+        radius * cos,
+        radius * sin,
+        0
+      )
+      normals.push(0, 0, 1)
+      uvs.push((cos + 1) * 0.5, (sin + 1) * 0.5)
+    }
+
+    for (let i = 2; i < vertices.length / 3; i++) {
+      indices.push(i - 1, i, 0)
+    }
+
+    mesh
+      .setIndices(
+        new Uint16Array(indices)
+      )
+      .setAttribute(
+        'position3d',
+        new Attribute(new Float32Array(vertices))
+      )
+      .setAttribute(
+        'normal3d',
+        new Attribute(new Float32Array(normals))
+      )
+      .setAttribute(
+        'uv',
+        new Attribute(new Float32Array(uvs))
+      )
+
+    return mesh
+  }
   
   static default() {
     return new Mesh()
