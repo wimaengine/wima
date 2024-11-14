8f07744684fd01ac3b1bfc75e749f93287119215
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2024-11-14 10:19:00 +0300

    [render] Changed signature of `Camera()` to (Projection,number,number)=>Camera.

--- src/render-core/components/camera.js
+++ src/render-core/components/camera.js
@@ -1,7 +1,14 @@
+import { Projection, PerspectiveProjection } from '../core/index.js'
+
 export class Camera {
 
   /**
    * @type {Projection}
    */
   projection
+  constructor(projection = new PerspectiveProjection(), near = 0.1, far = 1000) {
+    this.projection = projection
+    this.near = near
+    this.far = far
+  }
 }
