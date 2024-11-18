fbcf5e7a06feda7b66d5133926a80dbc4a3062ca
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2024-11-18 23:49:00 +0300

    [render] Added `GlDataType` to detemine webgl data types.

---
+++ src/render-webgl/core/constants/glparams.js
@@ -0,0 +1,13 @@
+/**
+ * @readonly
+ * @enum {number}
+ */
+export const GlDataType = {
+  Byte: 0x1400,
+  UnsignedByte: 0x1401,
+  Short: 0x1402,
+  UnsignedShort: 0x1403,
+  Int: 0x1404,
+  UnsignedInt: 0x1405,
+  Float: 0x1406
+}
