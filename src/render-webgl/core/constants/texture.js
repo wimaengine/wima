0f9e337cc464c2aeda80ad357c71966e82af3b33
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2024-11-20 09:43:00 +0300

    [render] Added `TextureFormat` to encode gpu supported texture formats.

---
+++ src/render-webgl/core/constants/texture.js
@@ -0,0 +1,27 @@
+/**
+ * @readonly
+ * @enum {number}
+ */
+export const TextureFormat = {
+
+  // gl.DEPTH_COMPONENT
+  Depth: 0x1902, 
+  Red: 0x1903,
+  Alpha: 0x1906,
+  RGB: 0x1907,
+  RGBA: 0x1908,
+  Luminance: 0x1909,
+  LuminanceAlpha: 0x190A,
+  RGB8: 0x8051,
+  RGBA8: 0x8058,
+  RGBA16F: 0x881A,
+  RGB16F: 0x881B,
+  RGBA32F: 0x8814,
+  RGB32F: 0x8815,
+  RGBA32I: 0x8D82,
+  RGB32I: 0x8D83,
+  RGBA16I: 0x8D88,
+  RGB16I: 0x8D89,
+  RGBA8I: 0x8D8E,
+  RGB8I: 0x8D8F
+}
