be4937b199d0bc4ea574167d6a5069f83d5e79e2
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2024-11-23 10:28:00 +0300

    [render] Added `createTexture()` to create webgl textures.

--- src/render-webgl/core/function.js
+++ src/render-webgl/core/function.js
@@ -70,3 +70,24 @@ export function validateShader(gl, shader, label = '') {
 
   return true
 }
+
+/**
+ * @param {WebGLRenderingContext} gl
+ * @param {TexImageSource} img
+ * @param {number} flipY
+ */
+export function createTexture(gl, img, flipY) {
+  const tex = gl.createTexture()
+
+  if (flipY) gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true)
+
+  gl.bindTexture(gl.TEXTURE_2D, tex)
+  gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, img)
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR)
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_NEAREST)
+  gl.generateMipmap(gl.TEXTURE_2D)
+
+  if (flipY) gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, false)
+
+  return tex
+}
