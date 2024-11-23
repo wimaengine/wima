3cdb94553ff737c87c155ae52bd5b6694226f9f4
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2024-11-23 10:27:00 +0300

    [render] Added `createBufferData()` to create webgl buffers with given data.

--- src/render-webgl/core/function.js
+++ src/render-webgl/core/function.js
@@ -18,3 +18,20 @@ export function createBuffer(gl, type, size, bufferUsage) {
 
   return buffer
 }
+
+/**
+ * @param {WebGLRenderingContext} gl
+ * @param {BufferType} type
+ * @param {TypeArray} data
+ * @param {DrawUsage} bufferUsage
+ * @returns {WebGLBuffer}
+ */
+export function createBufferData(gl, type, data, bufferUsage) {
+  const buffer = gl.createBuffer()
+
+  assert(buffer, 'Buffer could not be created.')
+  gl.bindBuffer(type, buffer)
+  gl.bufferData(type, data, bufferUsage)
+
+  return buffer
+}
