224d95f27cc3e57e1c730d080146540036fccfa9
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2024-11-26 16:00:00 +0300

    [render] Added `WebglBasicMaterial` for basic unlit rendering.

---
+++ src/render-webgl/assets/materials/basic.js
@@ -0,0 +1,92 @@
+
+import { Color } from '../../../math/color.js'
+import { Material } from '../../../render-core/index.js'
+import {
+  basicVertex as vertex,
+  basicFragment as fragment
+} from '../../shaders/index.js'
+
+export class WebglBasicMaterial extends Material {
+
+  /*
+   * @type {BasicMaterialFlags}
+   *
+   * flags = BasicMaterialFlags.None
+   */
+  /**
+   * @type {boolean}
+   */
+  enableVertexColors
+
+  /**
+   * @type {number}
+   */
+  opacity
+
+  /**
+   * @type {Color}
+   */
+  color
+  constructor({
+    enableVertexColors = false,
+    enableTransparency = false,
+    opacity = 1.0,
+    color = new Color(1, 1, 1, 1)
+
+    // flags = BasicMaterialFlags.None
+  } = {}) {
+    super()
+    this.color = color
+    this.enableVertexColors = enableVertexColors
+    this.enableTransparency = enableTransparency
+    this.opacity = opacity
+
+    // this.flags = flags
+  }
+
+  /**
+   * @returns {string}
+   */
+  vertex() {
+    return vertex
+  }
+
+  /**
+   * @returns {string}
+   */
+  fragment() {
+    return fragment
+  }
+  asUniformBind() {
+    return new Float32Array([
+      Number(this.enableVertexColors),
+      this.opacity,
+      0,
+      0,
+      this.color.r,
+      this.color.g,
+      this.color.b,
+      this.color.a
+    ])
+  }
+}
+
+
+// Removed this option as `WebglBasicMaterial.flags` as somehow webgl doesnt work well with bitflags.
+// May readd in the future if i find how to make it work.
+/*
+ * @readonly
+ * @enum {number}
+ *
+ * export const BasicMaterialFlags = {
+ * /**
+ * Default flag which doesnt enable any feature.
+ * /
+ * None: 0,
+ * /**
+ * Enables vertex colors
+ * /
+ * VertexColor: 1 << 0,
+ * Transparent: 1 << 1
+ * }
+ */
