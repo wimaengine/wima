68f8c3cd9c40096514baf20b705e7033f3d29a0b
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2025-01-20 10:45:00 +0300

    [math] Add `BVector2`
    A 2d vector of booleans.

---
+++ src/math/bvector2.js
@@ -0,0 +1,17 @@
+export class BVector2 {
+
+  /**
+   * @type {boolean}
+   */
+  x
+
+  /**
+   * @type {boolean}
+   */
+  y
+
+  constructor(x = false, y = false) {
+    this.x = x
+    this.y = y
+  }
+}
