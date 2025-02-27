5f877951503fda56a22ffd5d15b336e2c06f0a48
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2025-02-27 17:09:00 +0300

    [asset] Add `AssetParserPlugin<T>`

--- src/asset/plugins/parser.js
+++ src/asset/plugins/parser.js
@@ -1,5 +1,49 @@
+import { App, AppSchedule } from '../../app/index.js'
 import { Parser } from '../core/index.js'
+import { generateParserSystem } from '../systems/index.js'
 
+
+/**
+ * @template T
+ */
+
+export class AssetParserPlugin {
+
+  /**
+   * @readonly
+   * @type {new (...args:any)=>T}
+   */
+  asset
+
+  /**
+   * @readonly
+   * @type {Parser<T>}
+   */
+  parser
+
+  /**
+   * @param {AssetParserPluginOptions<T>} options 
+   */
+  constructor(options) {
+    const { asset, parser } = options
+
+    this.asset = asset
+    this.parser = parser
+  }
+
+  /**
+   * @param {App} app
+   */
+  register(app) {
+    const { asset, parser } = this
+    const name = asset.name.toLowerCase()
+
+    app
+      .registerSystem(AppSchedule.Update, generateParserSystem(name))
+      .getWorld()
+      .setResourceByName(`parser<${name}>`, parser)
+  }
+}
 
 /**
  * @template T
