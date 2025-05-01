3cbe2ecdafa5fe49a615e729cad2ecaf3cb22f4a
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2025-05-01 20:43:10 +0300

    Add `GlobalTransform3D` to `createMovable3D()`.

--- src/movable/prefabs/movable3d.js
+++ src/movable/prefabs/movable3d.js
@@ -1,11 +1,11 @@
-import { Orientation3D, Position3D, Scale3D } from '../../transform/index.js'
+import { GlobalTransform3D, Orientation3D, Position3D, Scale3D } from '../../transform/index.js'
 import { Velocity3D, Rotation3D, Acceleration3D, Torque3D } from '../components/index.js'
 
 /**
- * @returns {[Position3D,Orientation3D, Scale3D,Velocity3D,Rotation3D,Acceleration3D,Torque3D]}
+ * @returns {[Position3D,Orientation3D, Scale3D,GlobalTransform3D,Velocity3D,Rotation3D,Acceleration3D,Torque3D]}
  */
 export function createMovable3D() {
-  return [new Position3D(), new Orientation3D(), new Velocity3D(), new Scale3D(), new Rotation3D(), new Acceleration3D(), new Torque3D()]
+  return [new Position3D(), new Orientation3D(), new Scale3D(), new GlobalTransform3D(), new Velocity3D(), new Rotation3D(), new Acceleration3D(), new Torque3D()]
 }
 
 /**
