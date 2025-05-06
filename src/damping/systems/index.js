a91b15815d1adeda6602569e593a29a05e593f33
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2025-05-06 11:25:58 +0300

    Update `dampenVelocity3D()` system

--- src/damping/systems/index.js
+++ src/damping/systems/index.js
@@ -19,10 +19,7 @@ export function dampenVelocity2D(world) {
  * @param {World} world
  */
 export function dampenVelocity3D(world) {
-
-  /** @type {Query<[Velocity3D]>} */
-  const query = new Query(world, ['velocity3d'])
-
+  const query = new Query(world, [Velocity3D])
   const linear = 1 - world.getResource('lineardamping')
   
   query.each(([velocity]) => {
