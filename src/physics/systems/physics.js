fb8766716c890842a2f874e340e2103b8fdd8ee2
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2025-05-06 11:39:59 +0300

    Update `updateBodies()` system

--- src/physics/systems/physics.js
+++ src/physics/systems/physics.js
@@ -9,9 +9,7 @@ import { Orientation2D, Position2D, Scale2D } from '../../transform/index.js'
  * @param {World} world
  */
 export function updateBodies(world) {
-
-  /** @type {Query<[Position2D,Orientation2D,Scale2D,Collider2D]>} */
-  const query = new Query(world, ['position2d', 'orientation2d', 'scale2d', 'collider2d'])
+  const query = new Query(world, [Position2D, Orientation2D, Scale2D, Collider2D])
 
   query.each(([position, orientation, scale, shape]) => {
     Collider2D.update(
