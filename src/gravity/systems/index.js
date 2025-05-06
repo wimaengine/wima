80ec21d0546de418de4404f5452d6294fc6ae585
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2025-05-06 11:29:15 +0300

    Update `applyGravity2D()` system

--- src/gravity/systems/index.js
+++ src/gravity/systems/index.js
@@ -1,12 +1,14 @@
 import { Query, World } from '../../ecs/index.js'
 import { Vector2, Vector3 } from '../../math/index.js'
+import { Acceleration2D } from '../../movable/index.js'
+import { PhysicsProperties } from '../../physics/index.js'
 
 /**
  * @param {World} world
  */
 export function applyGravity2D(world) {
   const gravity = world.getResource('gravity2d')
-  const query = new Query(world, ['acceleration2d', 'physicsproperties'])
+  const query = new Query(world, [Acceleration2D, PhysicsProperties])
 
   query.each(([acceleration, properties]) => {
     if (properties.invmass === 0) return
