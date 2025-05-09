719591224d8b6f6ba2a8fda8e01f49e9595c4c78
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2025-05-09 18:03:02 +0300

    Update `setUpTouchEvents`

--- src/window-dom/core/touch.js
+++ src/window-dom/core/touch.js
@@ -7,28 +7,28 @@ import { TouchCancel, TouchEnd, TouchMove, TouchStart } from '../../window/index
  */
 export function setUpTouchEvents(world, target) {
   target.addEventListener('touchstart', (e) => {
-    const dispatch = world.getResource('events<touchstart>')
+    const dispatch = world.getResourceByName('events<touchstart>')
 
     for (let i = 0; i < e.changedTouches.length; i++) {
       dispatch.write(new TouchStart(e.changedTouches[i]))
     }
   })
   target.addEventListener('touchmove', (e) => {
-    const dispatch = world.getResource('events<touchmove>')
+    const dispatch = world.getResourceByName('events<touchmove>')
     
     for (let i = 0; i < e.changedTouches.length; i++) {
       dispatch.write(new TouchMove(e.changedTouches[i]))
     }
   })
   target.addEventListener('touchcancel', (e) => {
-    const dispatch = world.getResource('events<touchcancel>')
+    const dispatch = world.getResourceByName('events<touchcancel>')
 
     for (let i = 0; i < e.changedTouches.length; i++) {
       dispatch.write(new TouchCancel(e.changedTouches[i]))
     }
   })
   target.addEventListener('touchend', (e) => {
-    const dispatch = world.getResource('events<touchend>')
+    const dispatch = world.getResourceByName('events<touchend>')
 
     for (let i = 0; i < e.changedTouches.length; i++) {
       dispatch.write(new TouchEnd(e.changedTouches[i]))
