d0acbb2575c70bcc27dd380a59f0e406c8d8591c
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2025-05-09 17:30:54 +0300

    Update `collisionResponse` system.

--- src/physics/systems/physics.js
+++ src/physics/systems/physics.js
@@ -1,6 +1,6 @@
 import { PhysicsHitbox } from '../../broadphase/index.js'
 import { Query, World } from '../../ecs/index.js'
-import { CollisionManifold } from '../../narrowphase/index.js'
+import { CollisionManifold, Contacts } from '../../narrowphase/index.js'
 import { PhysicsSettings } from '../settings.js'
 import { Collider2D } from '../components/index.js'
 import { Orientation2D, Position2D, Scale2D } from '../../transform/index.js'
@@ -70,7 +70,7 @@ export function updateBounds(world) {
  */
 export function collisionResponse(world) {
   const invDt = 60
-  const contacts = world.getResource('contacts')
+  const contacts = world.getResource(Contacts)
 
   for (let i = 0; i < contacts.length; i++) {
     const {
