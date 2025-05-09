d81e392ac985cd594f36a7369fcb78d1d0564ee7
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2025-05-09 18:15:25 +0300

    Modify `Angular2DDamping`
    Adds a new property `Angular2DDamping.value` which
    contains the damping coefficient.

--- src/damping/resources/angulardampen.js
+++ src/damping/resources/angulardampen.js
@@ -1,2 +1,16 @@
-export class Angular2DDamping extends Number {}
+export class Angular2DDamping {
+
+  /**
+   * @type {number}
+   */
+  value
+
+  /**
+   * @param {number} value 
+   */
+  constructor(value = 0){
+    this.value = value
+  }
+}
+
 export class Angular3DDamping extends Number {}
