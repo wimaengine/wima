cea270ed89ae7c8b0c603f5aa426330102d45484
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2025-06-03 05:55:18 +0300

    Update `materialAddHook` component hook

--- src/render-webgl/hooks/material.js
+++ src/render-webgl/hooks/material.js
@@ -1,7 +1,7 @@
 /** @import {ComponentHook } from "../../ecs/index.js" */
 import { Query, Entity } from '../../ecs/index.js'
-import { Assets, Handle } from '../../asset/index.js'
-import { Material, ProgramCache, ShaderStage } from '../../render-core/index.js'
+import { Assets } from '../../asset/index.js'
+import { Material, MaterialHandle, ProgramCache, ShaderStage } from '../../render-core/index.js'
 import { createShader, createProgram, validateProgram, validateShader, WebglRenderPipeline } from '../core/index.js'
 import { MainWindow, Windows, Window } from '../../window/index.js'
 import { warn } from '../../logger/index.js'
@@ -14,13 +14,10 @@ import { AttributeMap, UBOCache } from '../resources/index.js'
  */
 export function materialAddHook(entity, world) {
 
-  /** @type {Handle<Material>} */
-  const handle = world.get(entity, 'materialhandle')
-
-  /** @type {AttributeMap} */
+  // SAFETY: Component is guaranteed as this is its component hook
+  const handle = /** @type {MaterialHandle}*/(world.get(entity, MaterialHandle))
+  
   const attributeMap = world.getResource(AttributeMap)
-
-  /** @type {UBOCache} */
   const ubos = world.getResource(UBOCache)
 
   /** @type {Assets<Material>} */
