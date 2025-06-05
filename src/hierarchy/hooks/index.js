eb7b128b7ac218b7d8815066560c76db9c5ab2f2
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2025-06-05 13:45:53 +0300

    Add `addSelfToParent` hook

---
+++ src/hierarchy/hooks/index.js
@@ -0,0 +1,28 @@
+/** @import {ComponentHook} from '../../ecs/index.js' */
+
+import { Children, Parent } from '../components/index.js'
+
+
+/**
+ * Component hook that ensures when a child 
+ * entity is spawned, the parent list is 
+ * updated.
+ * 
+ * @type {ComponentHook}
+ */
+export function addSelfToParent(entity, world) {
+  const parent = world.get(entity, Parent)
+
+  if (!parent) return
+
+  const children = world.get(parent.entity, Children)
+
+  if (children) {
+    children?.add(entity)
+  } else {
+
+    // Immediate insertion as insert commands on 
+    // `EntityCommands` will override each other.
+    world.insert(parent.entity, [new Children([entity])])
+  }
+}
