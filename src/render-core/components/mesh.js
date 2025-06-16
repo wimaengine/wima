56f803f4dc6faffbf7a08379675e63a687854286
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2025-06-16 20:59:56 +0300

    Add `Meshed`

--- src/render-core/components/mesh.js
+++ src/render-core/components/mesh.js
@@ -5,3 +5,18 @@ import { Mesh } from '../assets/index.js'
  * @augments {Handle<Mesh>}
  */
 export class MeshHandle extends Handle { }
+
+export class Meshed {
+
+  /**
+   * @type {Handle<Mesh>} 
+   */
+  handle
+
+  /**
+   * @param {Handle<Mesh>} handle 
+   */
+  constructor(handle) {
+    this.handle = handle
+  }
+}
