462a51c81c68ed9b2014f043b5fda3bf325ddaf1
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2025-06-17 11:56:18 +0300

    Add `Material2DPlugin`

---
+++ src/render-core/plugins/material.js
@@ -0,0 +1,59 @@
+/** @import { Constructor, TypeId } from '../../reflect/index.js' */
+
+import { App, AppSchedule, Plugin } from '../../app/index.js'
+import { typeidGeneric } from '../../reflect/index.js'
+import { Material } from '../assets/index.js'
+import { Material2D } from '../components/index.js'
+import { genBinRenderables2D } from '../systems/index.js'
+
+/**
+ * @template {Material} T
+ */
+export class Material2DPlugin extends Plugin {
+
+  /**
+   * @readonly
+   * @type {Constructor<Material2D<T>>}
+   */
+  component
+
+  /**
+   * @readonly
+   * @type {Constructor<T>}
+   */
+  asset
+
+  /**
+   * @param {Material2DPluginOptions<T>} param0 
+   */
+  constructor({ component, asset }) {
+    super()
+    this.asset = asset
+    this.component = component
+  }
+
+  /**
+   * @param {App} app
+   */
+  register(app) {
+    const { asset, component } = this
+
+    app
+      .registerType(component)
+      .registerSystem(AppSchedule.Update, genBinRenderables2D(asset, component))
+  }
+
+  /**
+   * @returns {TypeId}
+   */
+  name() {
+    return typeidGeneric(Material2DPlugin, [this.asset])
+  }
+}
+
+/**
+ * @template {Material} T
+ * @typedef Material2DPluginOptions
+ * @property {Constructor<Material2D<T>>} component
+ * @property {Constructor<T>} asset
+ */
