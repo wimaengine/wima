f0ea79d92908b94f7915313dd11d89b350bcf8de
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2025-06-17 11:58:23 +0300

    Add `Material3DPlugin`

--- src/render-core/plugins/material.js
+++ src/render-core/plugins/material.js
@@ -3,8 +3,8 @@
 import { App, AppSchedule, Plugin } from '../../app/index.js'
 import { typeidGeneric } from '../../reflect/index.js'
 import { Material } from '../assets/index.js'
-import { Material2D } from '../components/index.js'
-import { genBinRenderables2D } from '../systems/index.js'
+import { Material2D, Material3D } from '../components/index.js'
+import { genBinRenderables2D, genBinRenderables3D } from '../systems/index.js'
 
 /**
  * @template {Material} T
@@ -53,7 +53,59 @@ export class Material2DPlugin extends Plugin {
 
 /**
  * @template {Material} T
+ */
+export class Material3DPlugin extends Plugin {
+
+  /**
+   * @readonly
+   * @type {Constructor<Material3D<T>>}
+   */
+  component
+
+  /**
+   * @readonly
+   * @type {Constructor<T>}
+   */
+  asset
+
+  /**
+   * @param {Material3DPluginOptions<T>} param0 
+   */
+  constructor({ component, asset }) {
+    super()
+    this.asset = asset
+    this.component = component
+  }
+
+  /**
+   * @param {App} app
+   */
+  register(app) {
+    const { asset, component } = this
+
+    app
+      .registerType(component)
+      .registerSystem(AppSchedule.Update, genBinRenderables3D(asset, component))
+  }
+
+  /**
+   * @returns {TypeId}
+   */
+  name() {
+    return typeidGeneric(Material3DPlugin, [this.asset])
+  }
+}
+
+/**
+ * @template {Material} T
  * @typedef Material2DPluginOptions
  * @property {Constructor<Material2D<T>>} component
+ * @property {Constructor<T>} asset
+ */
+
+/**
+ * @template {Material} T
+ * @typedef Material3DPluginOptions
+ * @property {Constructor<Material3D<T>>} component
  * @property {Constructor<T>} asset
  */
