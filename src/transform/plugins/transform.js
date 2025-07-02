d0db7fe95206ddca20a7d69c4208f5fed69ddc4e
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2025-07-02 08:34:02 +0300

    Add `Transform3DPlugin`

--- src/transform/plugins/transform.js
+++ src/transform/plugins/transform.js
@@ -3,6 +3,10 @@ import {
     Orientation2D,
     Scale2D,
     GlobalTransform2D,
+    Position3D,
+    Orientation3D,
+    Scale3D,
+    GlobalTransform3D
 } from '../components/index.js'
 import { App, AppSchedule, Plugin } from '../../app/index.js'
 import { synctransform2D, synctransform3D } from '../systems/index.js'
@@ -18,5 +22,21 @@ export class Transform2DPlugin extends Plugin {
             .registerType(Scale2D)
             .registerType(GlobalTransform2D)
             .registerSystem(AppSchedule.Update, synctransform2D)
+    }
+}
+
+export class Transform3DPlugin extends Plugin {
+
+    /**
+     * @param {App} app
+     */
+    register(app) {
+        app
+            .registerType(Position3D)
+            .registerType(Orientation3D)
+            .registerType(Scale3D)
+            .registerType(GlobalTransform3D)
+            .registerSystem(AppSchedule.Update, synctransform3D)
+
     }
 }
