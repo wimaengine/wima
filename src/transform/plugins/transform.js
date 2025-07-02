b86740eca6f31a97cdeb1a566ecbfbc73ab8fff0
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2025-07-02 08:49:32 +0300

    Lint files

--- src/transform/plugins/transform.js
+++ src/transform/plugins/transform.js
@@ -1,42 +1,43 @@
 import {
-    Position2D,
-    Orientation2D,
-    Scale2D,
-    GlobalTransform2D,
-    Position3D,
-    Orientation3D,
-    Scale3D,
-    GlobalTransform3D
+  Position2D,
+  Orientation2D,
+  Scale2D,
+  GlobalTransform2D,
+  Position3D,
+  Orientation3D,
+  Scale3D,
+  GlobalTransform3D
 } from '../components/index.js'
 import { App, AppSchedule, Plugin } from '../../app/index.js'
 import { synctransform2D, synctransform3D } from '../systems/index.js'
 
 export class Transform2DPlugin extends Plugin {
-    /**
-     * @param {App} app
-     */
-    register(app) {
-        app
-            .registerType(Position2D)
-            .registerType(Orientation2D)
-            .registerType(Scale2D)
-            .registerType(GlobalTransform2D)
-            .registerSystem(AppSchedule.Update, synctransform2D)
-    }
+
+  /**
+   * @param {App} app
+   */
+  register(app) {
+    app
+      .registerType(Position2D)
+      .registerType(Orientation2D)
+      .registerType(Scale2D)
+      .registerType(GlobalTransform2D)
+      .registerSystem(AppSchedule.Update, synctransform2D)
+  }
 }
 
 export class Transform3DPlugin extends Plugin {
 
-    /**
-     * @param {App} app
-     */
-    register(app) {
-        app
-            .registerType(Position3D)
-            .registerType(Orientation3D)
-            .registerType(Scale3D)
-            .registerType(GlobalTransform3D)
-            .registerSystem(AppSchedule.Update, synctransform3D)
+  /**
+   * @param {App} app
+   */
+  register(app) {
+    app
+      .registerType(Position3D)
+      .registerType(Orientation3D)
+      .registerType(Scale3D)
+      .registerType(GlobalTransform3D)
+      .registerSystem(AppSchedule.Update, synctransform3D)
 
-    }
+  }
 }
--- src/transform/systems/transform.js
+++ src/transform/systems/transform.js
@@ -1,24 +1,24 @@
-import { Query,World } from "../../ecs/index.js"
-import { Position2D, Orientation2D, Scale2D, GlobalTransform2D, Position3D, Orientation3D, Scale3D, GlobalTransform3D } from "../components/index.js"
+import { Query, World } from '../../ecs/index.js'
+import { Position2D, Orientation2D, Scale2D, GlobalTransform2D, Position3D, Orientation3D, Scale3D, GlobalTransform3D } from '../components/index.js'
 
 /**
  * @param {World} world
  */
 export function synctransform2D(world) {
-    const query = new Query(world, [Position2D, Orientation2D, Scale2D, GlobalTransform2D])
+  const query = new Query(world, [Position2D, Orientation2D, Scale2D, GlobalTransform2D])
 
-    query.each(([position, orientation, scale, transform]) => {
-        transform.compose(position, orientation, scale)
-    })
+  query.each(([position, orientation, scale, transform]) => {
+    transform.compose(position, orientation, scale)
+  })
 }
 
 /**
  * @param {World} world
  */
 export function synctransform3D(world) {
-    const query = new Query(world, [Position3D, Orientation3D, Scale3D, GlobalTransform3D])
+  const query = new Query(world, [Position3D, Orientation3D, Scale3D, GlobalTransform3D])
 
-    query.each(([position, orientation, scale, transform]) => {
-        transform.compose(position, orientation, scale)
-    })
+  query.each(([position, orientation, scale, transform]) => {
+    transform.compose(position, orientation, scale)
+  })
 }
