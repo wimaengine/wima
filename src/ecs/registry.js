eed89cdd836b30ddf49785c2d8b2de9fb4d78088
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2025-07-03 07:48:26 +0300

    Rename `World.create` to `World.spawn`

--- src/ecs/registry.js
+++ src/ecs/registry.js
@@ -97,7 +97,7 @@ export class World {
    * @param {[...T]} components - The entity to add.
    * @returns {Entity}
    */
-  create(components) {
+  spawn(components) {
     const entityIndex = this.entities.reserve()
 
     // SAFETY: the entity was reserved in this function so we know its there.
