1cdb0c90d483f515ca449abcac479211c56809bd
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2025-07-07 11:30:26 +0300

    Arrange `RenderCorePlugin` internals

--- src/render-core/plugin.js
+++ src/render-core/plugin.js
@@ -1,6 +1,6 @@
 import { App, Plugin } from '../app/index.js'
 import { AssetParserPlugin, AssetPlugin } from '../asset/index.js'
-import { Camera, MaterialHandle, MeshHandle } from './components/index.js'
+import { Camera, MaterialHandle, MeshHandle, Meshed } from './components/index.js'
 import { Material, Mesh, Shader, Image } from './assets/index.js'
 import { ImageParser } from './resources/index.js'
 
@@ -11,26 +11,27 @@ export class RenderCorePlugin extends Plugin{
    */
   register(app) {
     app
-      .registerPlugin(new AssetPlugin({
-        asset:Image
-      }))
-      .registerPlugin( new AssetParserPlugin({
-        asset:Image,
-        parser:new ImageParser()
-      }))
-      .registerPlugin(new AssetPlugin({
-        asset:Shader
-      }))
-      .registerPlugin(new AssetPlugin({
-        asset:Mesh,
-        handleprovider:(id) => new MeshHandle(id)
-      }))
-      .registerPlugin(new AssetPlugin({
-        asset:Material,
-        handleprovider:(id) => new MaterialHandle(id)
-      }))
       .registerType(MeshHandle)
       .registerType(MaterialHandle)
+      .registerType(Meshed)
       .registerType(Camera)
+      .registerPlugin(new AssetPlugin({
+        asset: Image
+      }))
+      .registerPlugin(new AssetParserPlugin({
+        asset: Image,
+        parser: new ImageParser()
+      }))
+      .registerPlugin(new AssetPlugin({
+        asset: Mesh,
+        handleprovider: (id) => new MeshHandle(id)
+      }))
+      .registerPlugin(new AssetPlugin({
+        asset: Shader
+      }))
+      .registerPlugin(new AssetPlugin({
+        asset: Material,
+        handleprovider: (id) => new MaterialHandle(id)
+      }))
   }
 }
