52b8e92e2d4bb1e8c356d5ca67637522aa026e26
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2025-07-07 11:28:00 +0300

    Add `BasicMaterial3D`

--- src/render-core/components/materials/index.js
+++ src/render-core/components/materials/index.js
@@ -5,3 +5,8 @@ import { Material2D, Material3D } from '../material.js'
  * @augments Material2D<BasicMaterial>
  */
 export class BasicMaterial2D extends Material2D {}
+
+/**
+ * @augments Material3D<BasicMaterial>
+ */
+export class BasicMaterial3D extends Material3D {}
