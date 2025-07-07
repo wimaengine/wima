346ce4975dd533797f9efe473aa38e9d4ba9f4d7
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2025-07-07 11:33:21 +0300

    Register `BasicMaterial2D` to render basic material in 2d.

--- src/render-core/plugin.js
+++ src/render-core/plugin.js
@@ -1,10 +1,11 @@
 import { App, Plugin } from '../app/index.js'
 import { AssetParserPlugin, AssetPlugin } from '../asset/index.js'
-import { Camera, MaterialHandle, MeshHandle, Meshed } from './components/index.js'
-import { Material, Mesh, Shader, Image } from './assets/index.js'
+import { BasicMaterial2D, Camera, MaterialHandle, Meshed, MeshHandle } from './components/index.js'
+import { Material, Mesh, Shader, Image, BasicMaterial } from './assets/index.js'
 import { ImageParser } from './resources/index.js'
+import { Material2DPlugin } from './plugins/index.js'
 
-export class RenderCorePlugin extends Plugin{
+export class RenderCorePlugin extends Plugin {
 
   /**
    * @param {App} app 
@@ -35,6 +36,10 @@ export class RenderCorePlugin extends Plugin{
       }))
       .registerPlugin(new AssetPlugin({
         asset:BasicMaterial
+      }))
+      .registerPlugin(new Material2DPlugin({
+        asset: BasicMaterial,
+        component: BasicMaterial2D
       }))
   }
 }
