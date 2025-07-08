056890dafae73e2e21d6d1b6db3f56c51c9f6396
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2025-07-08 10:32:12 +0300

    Add glsl fragment shader for `BasicMaterial`

--- src/render-webgl/shaders/basic.js
+++ src/render-webgl/shaders/basic.js
@@ -15,3 +15,20 @@ void main(){
   gl_Position = camera.projection * camera.view * transpose(mat4(model)) * vec4(position3d,1.0);
 }
 `
+export const basicMaterial3DFragment =
+  `#version 300 es
+#pragma vscode_glsllint_stage: frag
+precision mediump float;
+
+uniform BasicMaterial {
+  vec4 color;
+} material;
+
+out vec4 output_color;
+
+void main(){
+  vec4 base_color = material.color;
+  
+  output_color = vec4(base_color.rgb,1.0);
+}
+`
