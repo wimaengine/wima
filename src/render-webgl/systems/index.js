e981d16af95b0693d699925025518b16eba96164
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2025-07-08 10:10:04 +0300

    Add `genRender` system generator

--- src/render-webgl/systems/index.js
+++ src/render-webgl/systems/index.js
@@ -1,12 +1,15 @@
 /** @import {SystemFunc} from '../../ecs/index.js' */
 /** @import { Constructor } from '../../reflect/index.js' */
+/** @import {UniformBind} from '../../render-core/index.js' */
 import { Entity, Query } from '../../ecs/index.js'
-import { warn } from '../../logger/index.js'
+import { assert, warn } from '../../logger/index.js'
 import { Windows, MainWindow, Window } from '../../window/index.js'
-import { typeid } from '../../reflect/index.js'
-import { AttributeMap, UBOCache, WebglProgramCache } from '../resources/index.js'
-import { ShaderStage } from '../../render-core/index.js'
+import { typeid, typeidGeneric } from '../../reflect/index.js'
+import { AttributeMap, ClearColor, MeshCache, UBOCache, WebglProgramCache } from '../resources/index.js'
+import { Camera, Material, Mesh, RenderLists3D, ShaderStage } from '../../render-core/index.js'
 import { WebglRenderPipeline, createShader, validateShader, createProgram, validateProgram } from '../core/index.js'
+import { Assets } from '../../asset/index.js'
+import { GlobalTransform3D } from '../../transform/index.js'
 
 /**
  * @template T
@@ -79,5 +82,103 @@ export function genRenderPipeline(type, vertexSource, fragmentSource) {
     ubos.create(gl, materialid, 16)
     pipe.init(gl, ubos)
     renderpipelines.set(materialid, pipe)
+  }
+}
+
+/**
+ * @template {Material & UniformBind} T
+ * @param {Constructor<T>} type
+ * @returns {SystemFunc}
+ */
+export function genRender(type) {
+  const materialtypeid = typeid(type)
+  const meshassetsid = typeidGeneric(Assets, [Mesh])
+  const materialassetsid = typeidGeneric(Assets, [type])
+
+  return function renderToWebgl(world) {
+
+    /** @type {Assets<Mesh>} */
+    const meshes = world.getResourceByTypeId(meshassetsid)
+
+    /** @type {Assets<T>} */
+    const materials = world.getResourceByTypeId(materialassetsid)
+
+    /** @type {MeshCache<WebGLVertexArrayObject>} */
+    const gpumeshes = world.getResourceByTypeId(typeid(MeshCache))
+    const programs = world.getResource(WebglProgramCache)
+    const ubos = world.getResource(UBOCache)
+    const canvases = world.getResource(Windows)
+    const windows = new Query(world, [Entity, Window, MainWindow])
+    const cameras = new Query(world, [GlobalTransform3D, RenderLists3D, Camera])
+    const clearColor = world.getResource(ClearColor)
+
+    const window = windows.single()
+
+    if (!window) return warn('Please define the main window for rendering.')
+
+    const canvas = canvases.getWindow(window[0])
+
+    if(!canvas) return
+
+    const gl = canvas.getContext('webgl2')
+
+    if (!gl) return warn('WebGL 2.0 context is not created or is lost.')
+
+    gl.clearColor(clearColor.r, clearColor.g, clearColor.b, clearColor.a)
+    gl.clear(gl.COLOR_BUFFER_BIT)
+
+    cameras.each(([transform, renderlist, camera]) => {
+      const projection = camera.projectionMatrix()
+      const cameraUniform = ubos.get('Camera')
+      const camData = new Float32Array([
+        ...transform.toMatrix4().inverse(),
+        ...projection
+      ])
+
+      assert(cameraUniform, 'The uniform buffer for the camera is not set up.')
+
+      gl.bindBuffer(gl.UNIFORM_BUFFER, cameraUniform.buffer)
+      gl.bufferSubData(gl.UNIFORM_BUFFER, 0, camData)
+
+      const pipeline = programs.get(materialtypeid)
+
+      if(!pipeline) return
+
+      const opaquepass = renderlist.getOpaquePass(materialtypeid)
+
+      if(!opaquepass) return
+      
+      for (let i = 0; i < opaquepass.length; i++) {
+        const { meshid, materialid, transform } = opaquepass[i]
+
+        const mesh = meshes.getById(meshid)
+        const material = materials.getById(materialid)
+        const gpumesh = gpumeshes.get(meshid)
+        const data = material.asUniformBind()
+        const ubo = ubos.get(materialtypeid)
+
+        assert(ubo, `The uniform buffer for material '${materialtypeid}' is not set up.`)
+
+        gl.useProgram(pipeline.program)
+        pipeline.setUniformMatrix3x4(gl, 'model', transform)
+        gl.bindBuffer(gl.UNIFORM_BUFFER, ubo.buffer)
+        gl.bufferSubData(gl.UNIFORM_BUFFER, 0, data)
+        gl.bindVertexArray(gpumesh)
+
+        const indices = mesh.getIndices()
+
+        if (indices) {
+          gl.drawElements(gl.TRIANGLES, indices.length, gl.UNSIGNED_SHORT, 0)
+        } else {
+          const positions = mesh.getAttribute('position3d')
+
+          if (positions === undefined) return
+
+          const count = positions.data.length
+
+          gl.drawArrays(gl.TRIANGLES, 0, count / 3)
+        }
+      }
+    })
   }
 }
