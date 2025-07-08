25e2e0016bfb7c5f4ae8dfde8fdbbfdd0cc55b62
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2025-07-08 10:05:37 +0300

    Add `WebglProgramCache`

---
+++ src/render-webgl/resources/programcache.js
@@ -0,0 +1,6 @@
+/** @import {TypeId} from "../../reflect/index.js". */
+
+import { WebglRenderPipeline } from '../core/renderpipeline.js'
+
+/** @augments Map<TypeId,WebglRenderPipeline> */
+export class WebglProgramCache extends Map {}
