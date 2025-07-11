9d32a7b20e8a5fb32f38484ca35193fc9737bfb3
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2025-07-11 14:28:24 +0300

    Rename `TWO_PI` to `TAU`

--- src/math/constants.js
+++ src/math/constants.js
@@ -1,5 +1,5 @@
 export const { PI } = Math
-export const TWO_PI = Math.PI * 2
+export const TAU = Math.PI * 2
 export const HALF_PI = Math.PI / 2
 export const DEG2RAD = Math.PI / 180
 export const epilson = Math.pow(2, -53)
