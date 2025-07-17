e6e153f08c1140b08b2ea897346b954488d79ad2
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2025-07-17 04:26:48 +0300

    Arrange constants

--- src/math/constants.js
+++ src/math/constants.js
@@ -1,7 +1,9 @@
-export const { PI } = Math
+import { sqrt } from './math.js'
+
 export const TAU = Math.PI * 2
+export const { PI } = Math
 export const HALF_PI = Math.PI / 2
 export const DEG2RAD = Math.PI / 180
 export const RAD2DEG = 180 / Math.PI
 export const epilson = 0.0000001
-export const SQRT2 = Math.sqrt(2)
+export const SQRT2 = sqrt(2)
