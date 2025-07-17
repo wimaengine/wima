a8d877fd27d72fd7340d34249e65cb68d838edb8
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2025-07-17 04:28:19 +0300

    Add `QUARTER_PI`

--- src/math/constants.js
+++ src/math/constants.js
@@ -3,6 +3,7 @@ import { sqrt } from './math.js'
 export const TAU = Math.PI * 2
 export const { PI } = Math
 export const HALF_PI = Math.PI / 2
+export const QUARTER_PI = Math.PI / 4
 export const DEG2RAD = Math.PI / 180
 export const RAD2DEG = 180 / Math.PI
 export const epilson = 0.0000001
