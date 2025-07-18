ee8a28f92257db0346b7087a820f9314d4132a11
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2025-07-18 10:00:09 +0300

    Add `BasicMaterialAssets` resource alias.

--- src/render-core/resources/aliases.js
+++ src/render-core/resources/aliases.js
@@ -1,7 +1,12 @@
 import { Assets } from '../../asset/index.js'
-import { Mesh } from '../assets/index.js'
+import { BasicMaterial, Mesh } from '../assets/index.js'
 
 /**
  * @augments {Assets<Mesh>}
  */
 export class MeshAssets extends Assets { }
+
+/**
+ * @augments {Assets<BasicMaterial>}
+ */
+export class BasicMaterialAssets extends Assets { }
