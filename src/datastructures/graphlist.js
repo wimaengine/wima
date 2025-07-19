3dcfb92fb825dd3292e8d93ae0af32b6a7e1a0ab
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2025-07-19 11:55:50 +0300

    Add `GraphEdge`

--- src/datastructures/graphlist.js
+++ src/datastructures/graphlist.js
@@ -20,3 +20,41 @@ export class GraphNode {
     this.weight = weight
   }
 }
+
+/**
+ * @template T
+ */
+export class GraphEdge {
+
+  /**
+   * @type {number}
+   */
+  to
+
+  /**
+   * @type {number}
+   */
+  from
+
+  /**
+   * @type {[EdgeId | undefined, EdgeId | undefined]}
+   */
+  next = [undefined, undefined]
+
+  /**
+   * @type {T}
+   */
+  weight
+
+  /**
+   * @param {number} from
+   * @param {number} to
+   * @param {T} weight 
+   */
+  constructor(from, to, weight) {
+    this.from = from
+    this.to = to
+    this.next = [undefined, undefined]
+    this.weight = weight
+  }
+}
