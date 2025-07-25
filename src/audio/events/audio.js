8c34b77ab819894d7f6967c1c765dce2a659db6c
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2025-07-25 09:19:31 +0300

    Add `AudioModified` asset event

--- src/audio/events/audio.js
+++ src/audio/events/audio.js
@@ -1,5 +1,8 @@
-import { AssetAdded } from '../../asset/index.js'
+import { AssetAdded, AssetModified } from '../../asset/index.js'
 import { Audio } from '../assets/index.js'
 
 /** @augments AssetAdded<Audio> */
 export class AudioAdded extends AssetAdded {}
+
+/** @augments AssetModified<Audio> */
+export class AudioModified extends AssetModified {}
