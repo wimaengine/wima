4570fb36366a670a0586f18fbcc76ff805911faa
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2025-07-25 17:07:24 +0300

    Add `BasicMaterialAdded` event

---
+++ src/render-core/events/basicmaterial.js
@@ -0,0 +1,5 @@
+import { AssetAdded } from '../../asset/index.js'
+import { BasicMaterial } from '../assets/index.js'
+
+/** @augments AssetAdded<BasicMaterial> */
+export class BasicMaterialAdded extends AssetAdded {}
