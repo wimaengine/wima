993a44795906e6f435d0774dcf988b14b022843f
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2025-07-25 17:05:26 +0300

    Add `ImageModified` event

--- src/render-core/events/image.js
+++ src/render-core/events/image.js
@@ -1,5 +1,8 @@
-import { AssetAdded } from '../../asset/index.js'
+import { AssetAdded, AssetModified } from '../../asset/index.js'
 import { Image } from '../assets/index.js'
 
 /** @augments AssetAdded<Image> */
 export class ImageAdded extends AssetAdded {}
+
+/** @augments AssetModified<Image> */
+export class ImageModified extends AssetModified {}
