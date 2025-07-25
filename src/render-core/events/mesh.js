f3c9ad7f6d6a99a9991710bb8c1f940a71f6b231
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2025-07-25 17:09:41 +0300

    Add `MeshModified` event

--- src/render-core/events/mesh.js
+++ src/render-core/events/mesh.js
@@ -1,5 +1,8 @@
-import { AssetAdded } from '../../asset/index.js'
+import { AssetAdded, AssetModified } from '../../asset/index.js'
 import { Mesh } from '../assets/index.js'
 
 /** @augments AssetAdded<Mesh> */
 export class MeshAdded extends AssetAdded {}
+
+/** @augments AssetModified<Mesh> */
+export class MeshModified extends AssetModified {}
