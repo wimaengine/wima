b872fdcc9432ca2b5553583826017fc37074bf86
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2025-07-30 22:10:58 +0300

    Add `transformRemote2D` system

---
+++ src/transform/systems/remote.js
@@ -0,0 +1,29 @@
+import { Query, World } from '../../ecs/index.js'
+import { Rotary } from '../../math/index.js'
+import { Position2D, Orientation2D, Scale2D, RemoteTransform2D } from '../components/index.js'
+
+
+/**
+ * @param {World} world
+ */
+export function transformRemote2D(world) {
+  const transforms = new Query(world, [Position2D, Orientation2D, Scale2D])
+  const remotes = new Query(world, [Position2D, Orientation2D, Scale2D, RemoteTransform2D])
+
+  remotes.each(([position, orientation, scale, remote]) => {
+    const entity = transforms.get(remote.entity)
+    const [offPosition, offOrientation, offScale] = remote.offsetTransform.decompose()
+
+    if (!entity) return
+
+    if (remote.copyTranslation) {
+      position.copy(entity[0]).add(offPosition)
+    }
+    if (remote.copyOrientation) {
+      orientation.value = entity[1].value + Rotary.toAngle(offOrientation)
+    }
+    if (remote.copyScale) {
+      scale.copy(entity[2]).multiply(offScale)
+    }
+  })
+}
