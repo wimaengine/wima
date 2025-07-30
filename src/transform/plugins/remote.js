f4c8da40f968bbf0fcd049d16e0ef20435c301ba
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2025-07-30 22:13:00 +0300

    Add `RemoteTransform3DPlugin`

--- src/transform/plugins/remote.js
+++ src/transform/plugins/remote.js
@@ -1,6 +1,6 @@
 import { AppSchedule, Plugin, App } from '../../app/index.js'
-import { transformRemote2D } from '../systems/index.js'
-import { RemoteTransform2D } from '../components/index.js'
+import { transformRemote2D, transformRemote3D } from '../systems/index.js'
+import { RemoteTransform3D, RemoteTransform2D } from '../components/index.js'
 
 export class RemoteTransform2DPlugin extends Plugin {
 
@@ -11,5 +11,17 @@ export class RemoteTransform2DPlugin extends Plugin {
     app
       .registerType(RemoteTransform2D)
       .registerSystem(AppSchedule.Update, transformRemote2D)
+  }
+}
+
+export class RemoteTransform3DPlugin extends Plugin {
+
+  /**
+   * @param {App} app 
+   */
+  register(app) {
+    app
+      .registerType(RemoteTransform3D)
+      .registerSystem(AppSchedule.Update, transformRemote3D)
   }
 }
