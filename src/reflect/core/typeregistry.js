98977f0cd5a068000959bda5096a663f11c57bab
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2025-08-13 05:39:18 +0300

    Add `TypeRegistry`

--- src/reflect/core/typeregistry.js
+++ src/reflect/core/typeregistry.js
@@ -1,4 +1,69 @@
+/** @import { TypeId, Constructor } from '../types/index.js' */
 import { TypeInfo } from './info.js'
+import { typeid } from './typeid.js'
+
+export class TypeRegistry {
+
+  /**
+   * @private
+   * @type {Map<TypeId,TypeEntry>}
+   */
+  inner = new Map()
+  
+  /**
+   * @template T
+   * @param {Constructor<T>} type
+   * @param {TypeInfo} info
+   */
+  register(type, info) {
+    const typeId = typeid(type)
+
+    this.registerTypeId(typeId, info)
+  }
+  
+  /**
+   * @param {TypeId} typeId
+   * @param {TypeInfo} info
+   */
+  registerTypeId(typeId, info) {
+    const entry = new TypeEntry(info)
+
+    this.inner.set(typeId, entry)
+  }
+  
+  /**
+   * @template T
+   * @param {Constructor<T>} type
+   */
+  unregister(type) {
+    const typeId = typeid(type)
+
+    this.unregisterTypeId(typeId)
+  }
+  
+  /**
+   * @param {TypeId} typeId
+   */
+  unregisterTypeId(typeId) {
+    this.inner.delete(typeId)
+  }
+  
+  /**
+   * @template T
+   * @param {Constructor<T>} type
+   * @returns {TypeEntry | undefined}
+   */
+  get(type) {
+    return this.getByTypeId(typeid(type))
+  }
+  
+  /**
+   * @param {TypeId} typeId
+   */
+  getByTypeId(typeId) {
+    return this.inner.get(typeId)
+  }
+}
 
 export class TypeEntry {
 
