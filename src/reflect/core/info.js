d70b19e9f6dbd163add3e88568b711cf3497a744
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2025-08-14 08:18:23 +0300

    Add `StructInfo`

--- src/reflect/core/info.js
+++ src/reflect/core/info.js
@@ -30,3 +30,65 @@ export class OpaqueInfo extends TypeInfo {
     return new OpaqueInfo()
   }
 }
+
+export class StructInfo extends TypeInfo {
+
+  /**
+   * @readonly
+   * @private
+   * @type {Map<string,number>}
+   */
+  names = new Map()
+  
+  /**
+   * @readonly
+   * @private
+   * @type {Field[]}
+   */
+  fields = []
+  
+  /**
+   * @param {Record<string,Field>} record
+   */
+  constructor(record) {
+    super()
+
+    for (const name in record) {
+      const position = this.fields.length
+
+      this.names.set(name, position)
+      this.fields.push(record[name])
+    }
+  }
+  static default() {
+    return new StructInfo({})
+  }
+
+  /**
+   * @param {string} name
+   */
+  get(name) {
+    const index = this.names.get(name)
+
+    if(index === undefined) return undefined
+
+    return this.getByIndex(index)
+  }
+
+  /**
+   * @param {number} index
+   */
+  getByIndex(index) {
+    return this.fields[index]
+  }
+  getFields() {
+    return this.fields
+  }
+  fieldNames() {
+    return this.names.keys()
+  }
+  
+  size(){
+    return this.fields.length
+  }
+}
