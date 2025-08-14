7b10c3db22750782145a2a1e1d7d0438057c6fa5
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2025-08-14 08:17:14 +0300

    Add `Field`

--- src/reflect/core/info.js
+++ src/reflect/core/info.js
@@ -1,1 +1,26 @@
+/** @import { TypeId } from '../types/index.js' */
 export class TypeInfo {}
+
+export class Field {
+
+  /**
+   * @readonly
+   * @type {TypeId}
+   */
+  type
+
+  /**
+   * @readonly
+   * @type {boolean}
+   */
+  optional
+
+  /**
+   * @param {TypeId} type
+   * @param {boolean} [optional=false]
+   */
+  constructor(type, optional = false) {
+    this.type = type
+    this.optional = optional
+  }
+}
