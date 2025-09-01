340777cae26912654f11bd28bac09cc7d5cbe542
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2025-09-01 18:14:00 +0300

    Update `Entities`

--- src/ecs/entities/entities.js
+++ src/ecs/entities/entities.js
@@ -1,9 +1,13 @@
+/** @import { ArchetypeId, TableRow } from '../typedef/index.js' */
 import { DenseList } from '../../datastructures/index.js'
 import { EntityLocation } from './location.js'
 
 /** @augments {DenseList<EntityLocation>} */
 export class Entities extends DenseList {
-  reserve(){
-    return this.push(new EntityLocation(-1, -1))
+  reserve() {
+    return this.push(new EntityLocation(
+      /** @type {ArchetypeId}*/(-1),
+      /** @type {TableRow}*/(-1)
+    ))
   }
 }
