24fd6b348252d035576201f42e6167820b992d98
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2025-09-01 18:14:00 +0300

    Add `EntityLocation.tableId`

--- src/ecs/entities/location.js
+++ src/ecs/entities/location.js
@@ -1,13 +1,31 @@
-/** @import {ArchetypeId} from '../typedef/index.js' */
+/** @import {ArchetypeId, TableId, TableRow} from '../typedef/index.js' */
 
 export class EntityLocation {
 
   /**
-   * @param {ArchetypeId} archid
-   * @param {number} index 
+   * @type {TableId}
    */
-  constructor(archid, index) {
+  tableId
+
+  /**
+   * @type {TableRow}
+   */
+  index
+
+  /**
+   * @param {ArchetypeId} archid
+   * @param {TableRow} index 
+   * @param {TableId} tableId
+   */
+  constructor(
+
+    // SAFETY: -1 Represents the invalid id for all of these identifiers
+    archid = /** @type { ArchetypeId }*/ (-1),
+    index = /** @type { TableRow }*/ (-1),
+    tableId = /** @type { TableId }*/ (-1)
+  ) {
     this.archid = archid
     this.index = index
+    this.tableId = tableId
   }
 }
