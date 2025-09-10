9a2428bfcdd25dd1dce829d2a9f3c771c2c2a5de
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2025-09-10 18:27:28 +0300

    Add `Has` query filter

--- src/ecs/query/filters/filter.js
+++ src/ecs/query/filters/filter.js
@@ -1,4 +1,5 @@
-/** @import { TypeId } from '../../../reflect/index.js'*/
+/** @import { TypeId, Constructor } from '../../../reflect/index.js'*/
+import { typeid } from '../../../reflect/index.js'
 
 /**
  * @interface
@@ -12,5 +13,32 @@ export class QueryFilter {
    */
   archetype(_types){
     throw `Implement ${this.constructor.name}.archetype`
+  }
+}
+
+/**
+ * @template T
+ * @implements {QueryFilter}
+ */
+export class Has {
+
+  /**
+   * @type {TypeId}
+   */
+  typeid
+
+  /**
+   * @param {Constructor<T>} component
+   */
+  constructor(component){
+    this.typeid = typeid(component)
+  }
+
+  /**
+   * @param {readonly TypeId[]} types 
+   * @returns {boolean}
+   */
+  archetype(types){
+    return types.includes(this.typeid)
   }
 }
