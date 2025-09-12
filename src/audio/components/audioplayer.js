785de3d9c636846cee99e9e09d726eb9637aa741
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2025-09-12 14:20:27 +0300

    Add `AudioPlayer`

---
+++ src/audio/components/audioplayer.js
@@ -0,0 +1,69 @@
+/** @import { NodeId } from '../../datastructures/index.js' */
+/** @import { ComponentHook } from '../../ecs/index.js' */
+import { Handle } from '../../asset/index.js'
+import { Timer, TimerMode } from '../../time/index.js'
+import { Audio } from '../assets/index.js'
+import { AudioGraph } from '../resources/index.js'
+
+export class AudioPlayer {
+
+  /**
+   * @type {NodeId | undefined}
+   */
+  sourceNode
+
+  /**
+   * @type {NodeId | undefined}
+   */
+  attach
+
+  /**
+   * @type {Handle<Audio> | undefined}
+   */
+  audio
+
+  /**
+   * @type {Timer}
+   */
+  playback = new Timer(1000000)
+
+  /**
+   * @param {AudioPlayerOptions} [options]
+   */
+  constructor({ attach, audio, playbackMode = TimerMode.Once } = {}) {
+    this.attach = attach
+    this.audio = audio
+    this.playback.mode = playbackMode
+  }
+}
+
+/**
+ * @type {ComponentHook}
+ */
+export function removeAudioPlayerSink(entity, world) {
+  const graph = world.getResource(AudioGraph)
+  const audio = world.get(entity, AudioPlayer)
+
+  if(!audio){
+    return
+  }
+  
+  audio.audio?.drop()
+
+  // SAFETY: The node referenced by the player is guaranteed to be a `AudioBufferSourceNode`.
+  const node = /** @type {AudioBufferSourceNode | undefined} */(graph.graph.getNode(audio.sourceNode)?.weight)
+  
+  if(node){
+    node.stop()
+
+    // TODO: Remove the audio sink from the graph when removing nodes on a graph
+    // is available.
+  }
+}
+
+/**
+ * @typedef AudioPlayerOptions
+ * @property {NodeId} [attach]
+ * @property {Handle<Audio>} [audio]
+ * @property {TimerMode} [playbackMode]
+ */
