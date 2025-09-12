9f11bfed9a9fa5c1fe3c83b29b66d97063e2a0a3
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2025-09-12 14:21:36 +0300

    Add `playAudio` system

---
+++ src/audio/systems/index.js
@@ -0,0 +1,65 @@
+import { Query, World } from '../../ecs/index.js'
+import { TimerMode } from '../../time/index.js'
+import { AudioAssets, AudioGraph } from '../resources/index.js'
+import { AudioPlayer } from '../components/index.js'
+
+
+/**
+ * @param {World} world
+ */
+export function playAudio(world) {
+  const graph = world.getResource(AudioGraph)
+  const audioSources = world.getResource(AudioAssets)
+  const players = new Query(world, [AudioPlayer])
+  const ctx = graph.getContext()
+  const root = graph.getRoot()
+  
+  
+  players.each(([player]) => {
+    const { audio, sourceNode, playback, attach } = player
+
+    if (!audio) return
+
+    const source = audioSources.get(audio)
+
+    if (!source) {
+      return
+    }
+    if (sourceNode) {
+
+      // update node if a play/pause or start/stop is requested
+    } else {
+      const node = new AudioBufferSourceNode(ctx, {
+        buffer: source.audiobuffer,
+        loop: looped(playback.mode)
+      })
+      const id = graph.add(node)
+
+      if (attach) {
+        graph.connect(id, attach)
+      } else {
+        graph.connect(id, root)
+      }
+
+      node.start(0, playback.elapsed)
+      playback.duration = source.audiobuffer.duration
+      player.sourceNode = id
+    }
+  })
+}
+
+/**
+ * @param {TimerMode} playbackMode
+ */
+function looped(playbackMode) {
+  switch (playbackMode) {
+    case TimerMode.Once:
+      return false
+
+    case TimerMode.Repeat:
+      return true
+
+    default:
+      return false
+  }
+}
