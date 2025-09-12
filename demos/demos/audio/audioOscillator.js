39b9771b0fa822f7cd7384ce6a0338cbe0c5bbc8
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2025-09-12 14:42:23 +0300

    Add audio oscillator demo

---
+++ demos/demos/audio/audioOscillator.js
@@ -0,0 +1,28 @@
+import {
+  Demo,
+  World,
+  AudioOscillator,
+  EntityCommands,
+  Cleanup
+} from 'wima'
+import { addDefaultCamera3D } from '../utils.js'
+
+export default new Demo(
+  'audio/audio oscillator',
+  [init, addDefaultCamera3D]
+)
+
+/**
+ * @param {World} world
+ */
+function init(world) {
+  const commands = world.getResource(EntityCommands)
+  
+  commands
+    .spawn()
+    .insertPrefab([
+      new AudioOscillator(),
+      new Cleanup()
+    ])
+    .build()
+}
--- demos/demos/audio/index.js
+++ demos/demos/audio/index.js
@@ -1,2 +1,3 @@
 export { default as audioGraph } from './audioGraph.js'
 export { default as audioPlayer } from './audioPlayer.js'
+export { default as audioOscillator } from './audioOscillator.js'
