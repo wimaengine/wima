f3de9dceccbc2b290dd4c7e2b25e43c33d4895e7
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2025-09-17 01:08:13 +0300

    Move scheduling functionality to `schedule` package

--- src/ecs/schedule/index.js
+++
@@ -1,4 +0,0 @@
-export * from './schedule.js'
-export * from './executors/index.js'
-export * from './scheduler.js'
-export * from './executable.js'
--- src/schedule/core/systembuilder.js
+++ src/schedule/core/systembuilder.js
@@ -1,6 +1,6 @@
-import { Scheduler } from '../../ecs/index.js'
+import { Scheduler } from '../index.js'
 import { assert } from '../../logger/index.js'
-import { SystemConfig } from './systemconfig.js'
+import { SystemConfig } from '../../app/core/systemconfig.js'
 
 export class SchedulerBuilder {
 
@@ -26,7 +26,7 @@ export class SchedulerBuilder {
 
       const schedule = scheduler.get(config.schedule)
 
-      assert(schedule, `The schedule label ${scheduler} is not set in the provided \`Scheduler\`.`)
+      assert(schedule, `The schedule label "${config.schedule}" is not set in the provided \`Scheduler\`.`)
 
       schedule.add(config.system)
     }
