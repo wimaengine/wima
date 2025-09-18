7eff16e5828c19df009d88ebc11db9d982034fe9
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2025-09-18 07:46:57 +0300

    Add `updateTimers` system

---
+++ src/time/systems/timer.js
@@ -0,0 +1,17 @@
+import { Query, World } from '../../ecs/index.js'
+import { Timer } from '../components/index.js'
+import { VirtualClock } from '../resource/index.js'
+
+
+/**
+ * @param {World} world
+ */
+export function updateTimers(world) {
+  const timers = new Query(world, [Timer])
+  const clock = world.getResource(VirtualClock)
+  const delta = clock.getDelta()
+  
+  timers.each(([timer]) => {
+    timer.update(delta)
+  })
+}
