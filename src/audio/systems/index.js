f22c1339c52d0f01de71594e343da59a05255b01
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2025-09-19 10:55:00 +0300

    Update `playAudio` system

--- src/audio/systems/index.js
+++ src/audio/systems/index.js
@@ -1,5 +1,5 @@
 import { Query, World } from '../../ecs/index.js'
-import { VirtualClock, TimerMode } from '../../time/index.js'
+import { Timer, TimerMode } from '../../time/index.js'
 import { AudioAssets, AudioGraph } from '../resources/index.js'
 import { AudioPlayer, AudioOscillator, AudioOscillatorType } from '../components/index.js'
 
@@ -10,13 +10,12 @@ import { AudioPlayer, AudioOscillator, AudioOscillatorType } from '../components
 export function playAudio(world) {
   const graph = world.getResource(AudioGraph)
   const audioSources = world.getResource(AudioAssets)
-  const players = new Query(world, [AudioPlayer])
+  const players = new Query(world, [AudioPlayer, Timer])
   const ctx = graph.getContext()
   const root = graph.getRoot()
-  
-  
-  players.each(([player]) => {
-    const { audio, sourceNode, playback, attach } = player
+
+  players.each(([player, playback]) => {
+    const { audio, sourceNode, attach } = player
 
     if (!audio) return
 
@@ -26,14 +25,31 @@ export function playAudio(world) {
       return
     }
     if (sourceNode) {
+      if (playback.completed()) {
+        graph.update(sourceNode, undefined)
+      }
 
-      // update node if a play/pause or start/stop is requested
-    } else {
+      if (!playback.playbackChanged()) return
+      if (playback.paused) {
+        graph.update(sourceNode, undefined)
+
+        return
+      }
+
       const node = new AudioBufferSourceNode(ctx, {
         buffer: source.audiobuffer,
-        loop: looped(playback.mode)
+        loop: looped(playback.mode),
+        playbackRate: playback.speed
       })
-      const id = graph.add(node)
+
+      node.start(0, playback.elapsed())
+      graph.update(sourceNode, node)
+
+    } else {
+
+      // The Oscillator will play in the next frame..
+      // TODO: Maybe do this in a component hook?
+      const id = graph.add(undefined)
 
       if (attach) {
         graph.connect(id, attach)
@@ -41,8 +57,11 @@ export function playAudio(world) {
         graph.connect(id, root)
       }
 
-      node.start(0, playback.elapsed())
+      const elapsed = playback.elapsed()
+
       playback.duration = source.audiobuffer.duration
+      playback.reset()
+      playback.seek(elapsed)
       player.sourceNode = id
     }
   })
