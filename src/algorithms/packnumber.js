89720c16750b6e15b6f22fdedc3e6c6f1fd345cf
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2025-09-26 03:44:47 +0300

    Add `unpackFrom64Int`

--- src/algorithms/packnumber.js
+++ src/algorithms/packnumber.js
@@ -18,3 +18,25 @@ export function packInto64Int(low, high) {
   // manipulating "integers"
   return (high * 2 ** 32) + low
 }
+
+/**
+ * Unpacks two numbers from a single number.
+ * 
+ * #### Note
+ * This operation is should be analogous to unpacking two 32 bit
+ * numbers from 64 bit number in the high and low bits.Due
+ * to language limitations, the number passed into this function
+ * can only be up to 2 ^ 53, otherwise the output will be garbled
+ * nonsense.
+ * 
+ * @param {number} value 
+ */
+export function unpackFrom64Int(value) {
+
+  // We cant use bit manipulation as javascript has a 32 bit limit on
+  // manipulating "integers"
+  const low = value % 2 ** 32
+  const high = Math.floor(value / 2 ** 32)
+
+  return [low, high]
+}
