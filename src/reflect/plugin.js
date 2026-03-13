2414f64346e6e75e53ec72d0f0d17821fb79cbbe
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2026-03-13 17:04:37 +0300

    Register primitive types to type registry

--- src/reflect/plugin.js
+++ src/reflect/plugin.js
@@ -1,4 +1,6 @@
 import { App, Plugin } from "../app/index.js";
+import { OpaqueInfo } from "./core/info.js";
+import { setTypeId } from "./core/typeid.js";
 import { TypeRegistry } from "./core/typeregistry.js";
 
 export class ReflectPlugin extends Plugin {
@@ -6,6 +8,15 @@ export class ReflectPlugin extends Plugin {
      * @param {App} app
      */
     register(app){
-        app.setResource(new TypeRegistry())
+        const typeregistry = new TypeRegistry()
+
+        typeregistry.register(String, new OpaqueInfo())
+        typeregistry.register(Number, new OpaqueInfo())
+        typeregistry.register(Boolean, new OpaqueInfo())
+        typeregistry.registerTypeId(setTypeId("BigInt"), new OpaqueInfo())
+
+        console.log(typeregistry);
+        
+        app.setResource(typeregistry)
     }
 }
