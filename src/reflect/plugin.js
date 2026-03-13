af6c455eba0de00c6daac58da4883a28869859f7
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2026-03-13 18:11:51 +0300

    Lint files

--- src/reflect/plugin.js
+++ src/reflect/plugin.js
@@ -1,22 +1,23 @@
-import { App, Plugin } from "../app/index.js";
-import { OpaqueInfo } from "./core/info.js";
-import { setTypeId } from "./core/typeid.js";
-import { TypeRegistry } from "./core/typeregistry.js";
+import { App, Plugin } from '../app/index.js'
+import { OpaqueInfo } from './core/info.js'
+import { setTypeId } from './core/typeid.js'
+import { TypeRegistry } from './core/typeregistry.js'
 
 export class ReflectPlugin extends Plugin {
-    /**
-     * @param {App} app
-     */
-    register(app){
-        const typeregistry = new TypeRegistry()
 
-        typeregistry.register(String, new OpaqueInfo())
-        typeregistry.register(Number, new OpaqueInfo())
-        typeregistry.register(Boolean, new OpaqueInfo())
-        typeregistry.registerTypeId(setTypeId("BigInt"), new OpaqueInfo())
+  /**
+   * @param {App} app
+   */
+  register(app) {
+    const typeregistry = new TypeRegistry()
 
-        console.log(typeregistry);
-        
-        app.setResource(typeregistry)
-    }
+    typeregistry.register(String, new OpaqueInfo())
+    typeregistry.register(Number, new OpaqueInfo())
+    typeregistry.register(Boolean, new OpaqueInfo())
+    typeregistry.registerTypeId(setTypeId('BigInt'), new OpaqueInfo())
+
+    console.log(typeregistry)
+
+    app.setResource(typeregistry)
+  }
 }
