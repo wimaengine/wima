3bb2e07d7d216c1d28ed60654222a0cbc83e8264
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2026-03-19 07:46:54 +0300

    Simplify `Scheduler`

---
+++ src/schedule/core/runner.js
@@ -0,0 +1,9 @@
+/**
+ * @typedef {(scheduler: Scheduler, world: World) => void} Runner
+ */
+
+import { World } from "../../ecs/index.js";
+import { Scheduler } from "./scheduler.js";
+
+
+export default {}
