756b5bbd4710365683efc60f9fa187fb29edcb51
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2026-03-22 01:00:00 +0300

    Register types of name into the type registry

--- src/name/plugin.js
+++ src/name/plugin.js
@@ -1,5 +1,7 @@
 import { App, Plugin } from '../app/index.js'
+import { AppSchedule } from '../core/index.js'
 import { Name } from './components/index.js'
+import { registerNameTypes } from './systems/index.js'
 
 export class NamePlugin extends Plugin {
 
@@ -7,6 +9,8 @@ export class NamePlugin extends Plugin {
    * @param {App} app
    */
   register(app) {
-    app.registerType(Name)
+    app
+      .registerType(Name)
+      .registerSystem(AppSchedule.Startup, registerNameTypes)
   }
 }
---
+++ src/name/systems/types.js
@@ -0,0 +1,16 @@
+import { World } from '../../ecs/index.js'
+import { Field, StructInfo } from '../../reflect/core/index.js'
+import { TypeRegistry } from '../../reflect/resources/index.js'
+import { typeid } from '../../type/index.js'
+import { Name } from '../components/index.js'
+
+/**
+ * @param {World} world
+ */
+export function registerNameTypes(world) {
+  const registry = world.getResource(TypeRegistry)
+
+  registry.register(Name, new StructInfo({
+    value: new Field(typeid(String))
+  }))
+}
