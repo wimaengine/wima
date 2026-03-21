0e50c95e52d8344193290854d815725e8e70044e
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2026-03-21 07:00:00 +0300

    Register types of geometry into the type registry

---
+++ src/geometry/systems/types.js
@@ -0,0 +1,28 @@
+import { World } from '../../ecs/index.js'
+import { EnumInfo, Field, StructInfo } from '../../reflect/core/index.js'
+import { TypeRegistry } from '../../reflect/resources/index.js'
+import { setTypeId, typeid } from '../../type/index.js'
+import { BoundingBox2D, BoundingCircle } from '../AABB/index.js'
+import { BoundType } from '../AABB/boundtype.js'
+import { Vector2 } from '../../math/index.js'
+
+/**
+ * @param {World} world
+ */
+export function registerGeometryTypes(world) {
+  const registry = world.getResource(TypeRegistry)
+
+  const boundTypeId = setTypeId('BoundType')
+  registry.registerTypeId(boundTypeId, new EnumInfo(BoundType))
+
+  registry.register(BoundingBox2D, new StructInfo({
+    type: new Field(boundTypeId),
+    max: new Field(typeid(Vector2)),
+    min: new Field(typeid(Vector2))
+  }))
+  registry.register(BoundingCircle, new StructInfo({
+    type: new Field(boundTypeId),
+    r: new Field(typeid(Number)),
+    pos: new Field(typeid(Vector2))
+  }))
+}
