0352d65bfe000412257aee7988137769ff0a7442
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2026-03-22 13:00:00 +0300

    Register types of render-core into the type registry

--- src/render-core/systems/index.js
+++ src/render-core/systems/index.js
@@ -1,1 +1,2 @@
 export * from './bin.js'
+export * from './types.js'
---
+++ src/render-core/systems/types.js
@@ -0,0 +1,83 @@
+/** @import { Constructor } from '../../type/index.js' */
+
+import { World } from '../../ecs/index.js'
+import { EnumInfo, Field, MapInfo, StructInfo } from '../../reflect/core/index.js'
+import { TypeRegistry } from '../../reflect/resources/index.js'
+import { setTypeId, typeid, typeidGeneric } from '../../type/index.js'
+import { Handle } from '../../asset/index.js'
+import { Color } from '../../color/index.js'
+import { Vector2 } from '../../math/index.js'
+import { Camera, Meshed } from '../components/index.js'
+import { BasicMaterial2D, BasicMaterial3D } from '../components/materials/index.js'
+import { MeshAttributeData } from '../core/attributedata.js'
+import { Projection, ShaderStage } from '../core/index.js'
+import { BasicMaterial, Image, Material, Mesh, Shader } from '../assets/index.js'
+
+/**
+ * @param {World} world
+ */
+export function registerRenderCoreTypes(world) {
+  const registry = world.getResource(TypeRegistry)
+
+  const meshAttributeMapId = typeidGeneric(Map, [String, MeshAttributeData])
+  const shaderStageId = setTypeId('ShaderStage')
+  const basicMaterialHandleId = typeidGeneric(Handle, [BasicMaterial])
+  registry.registerTypeId(meshAttributeMapId, new MapInfo(typeid(String), typeid(MeshAttributeData)))
+  registry.registerTypeId(shaderStageId, new EnumInfo(ShaderStage))
+
+  registry.register(MeshAttributeData, new StructInfo({
+    data: new Field(typeid(Float32Array))
+  }))
+  registry.register(Image, new StructInfo({
+    raw: new Field(typeid(Uint8ClampedArray)),
+    dimensions: new Field(typeid(Vector2))
+  }))
+  registry.register(Mesh, new StructInfo({
+    indices: new Field(typeid(Uint16Array), true),
+    attributes: new Field(meshAttributeMapId)
+  }))
+  registry.register(Shader, new StructInfo({
+    stage: new Field(shaderStageId),
+    source: new Field(typeid(String))
+  }))
+  registry.register(BasicMaterial, new StructInfo({
+    color: new Field(typeid(Color))
+  }))
+  registry.register(Meshed, new StructInfo({
+    handle: new Field(typeidGeneric(Handle,[Mesh]))
+  }))
+  registry.register(BasicMaterial2D, new StructInfo({
+    handle: new Field(basicMaterialHandleId)
+  }))
+  registry.register(BasicMaterial3D, new StructInfo({
+    handle: new Field(basicMaterialHandleId)
+  }))
+  registry.register(Camera, new StructInfo({
+    projection: new Field(typeid(Projection)),
+    near: new Field(typeid(Number)),
+    far: new Field(typeid(Number))
+  }))
+}
+
+/**
+ * @template T
+ * @template {Material} U
+ * @param {Constructor<T>} component
+ * @param {Constructor<U>} material
+ * @returns {import('../../ecs/index.js').SystemFunc}
+ */
+export function registerMaterialTypes(component, material) {
+  return function registerMaterialTypes(world) {
+    const registry = world.getResource(TypeRegistry)
+
+    const handleTypeId = typeidGeneric(Handle, [material])
+    registry.registerTypeId(handleTypeId, new StructInfo({
+      type: new Field(typeid(Function)),
+      index: new Field(typeid(Number)),
+      generation: new Field(typeid(Number))
+    }))
+    registry.register(component, new StructInfo({
+      handle: new Field(handleTypeId)
+    }))
+  }
+}
