d831f68956a89bafba90b8b49626d8859c23613a
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2026-03-28 20:38:18 +0300

    Add static copy and clone methods on `Rotation2D`

--- src/movable/components/2d/rotation.js
+++ src/movable/components/2d/rotation.js
@@ -1,3 +1,21 @@
 import { Angle } from '../../../math/index.js'
 
-export class Rotation2D extends Angle {}
+export class Rotation2D extends Angle {
+
+  /**
+   * @param {Rotation2D} source
+   * @param {Rotation2D} target
+   */
+  static copy(source, target = new Rotation2D()) {
+    target.value = source.value
+
+    return target
+  }
+
+  /**
+   * @param {Rotation2D} target
+   */
+  static clone(target) {
+    return this.copy(target)
+  }
+}
