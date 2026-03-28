080d98956f7975786d5fd687e50fca46add5e48e
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2026-03-28 20:38:19 +0300

    Add static copy and clone methods on `MainWindow`

--- src/window/components/main.js
+++ src/window/components/main.js
@@ -1,1 +1,17 @@
-export class MainWindow {}
+export class MainWindow {
+
+  /**
+   * @param {MainWindow} source
+   * @param {MainWindow} target
+   */
+  static copy(source, target = new MainWindow()) {
+    return target
+  }
+
+  /**
+   * @param {MainWindow} target
+   */
+  static clone(target) {
+    return this.copy(target)
+  }
+}
