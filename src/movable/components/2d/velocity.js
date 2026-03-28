5f4e98343e07a517039a23f0d54b6b8e7b43fb4f
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2026-03-28 20:38:18 +0300

    Add static copy and clone methods on `Velocity2D`

--- src/movable/components/2d/velocity.js
+++ src/movable/components/2d/velocity.js
@@ -1,3 +1,19 @@
 import { Vector2 } from '../../../math/index.js'
 
-export class Velocity2D extends Vector2 {}
+export class Velocity2D extends Vector2 {
+
+  /**
+   * @param {Velocity2D} source
+   * @param {Velocity2D} target
+   */
+  static copy(source, target = new Velocity2D()) {
+    return super.copy(source, target)
+  }
+
+  /**
+   * @param {Velocity2D} target
+   */
+  static clone(target) {
+    return this.copy(target)
+  }
+}
