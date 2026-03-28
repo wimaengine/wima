97cbf8bdde5130228061127ff803b198bac993f0
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2026-03-28 20:38:19 +0300

    Add static copy and clone methods on `GlobalTransform3D`

--- src/transform/components/3d/globaltransform.js
+++ src/transform/components/3d/globaltransform.js
@@ -1,3 +1,19 @@
 import { Affine3 } from '../../../math/index.js'
 
-export class GlobalTransform3D extends Affine3 { }
+export class GlobalTransform3D extends Affine3 {
+
+  /**
+   * @param {GlobalTransform3D} source
+   * @param {GlobalTransform3D} target
+   */
+  static copy(source, target = new GlobalTransform3D()) {
+    return super.copy(source, target)
+  }
+
+  /**
+   * @param {GlobalTransform3D} target
+   */
+  static clone(target) {
+    return this.copy(target)
+  }
+}
