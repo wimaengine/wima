6423293647974bb9b868ed1026c80f446f19cbb9
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2026-03-28 20:38:18 +0300

    Add static copy and clone methods on `Torque3D`

--- src/movable/components/3d/torque.js
+++ src/movable/components/3d/torque.js
@@ -1,3 +1,19 @@
 import { Vector3 } from '../../../math/index.js'
 
-export class Torque3D extends Vector3 {}
+export class Torque3D extends Vector3 {
+
+  /**
+   * @param {Torque3D} source
+   * @param {Torque3D} target
+   */
+  static copy(source, target = new Torque3D()) {
+    return super.copy(source, target)
+  }
+
+  /**
+   * @param {Torque3D} target
+   */
+  static clone(target) {
+    return this.copy(target)
+  }
+}
