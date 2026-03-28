80d6ff4bfdfd66fcdce9e7735e69f4dff0ee9ab3
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2026-03-28 20:38:19 +0300

    Add static copy and clone methods on `GlobalTransform2D`

--- src/transform/components/2d/globaltransform.js
+++ src/transform/components/2d/globaltransform.js
@@ -1,3 +1,19 @@
 import { Affine2 } from '../../../math/index.js'
 
-export class GlobalTransform2D extends Affine2 {}
+export class GlobalTransform2D extends Affine2 {
+
+  /**
+   * @param {GlobalTransform2D} source
+   * @param {GlobalTransform2D} target
+   */
+  static copy(source, target = new GlobalTransform2D()) {
+    return super.copy(source, target)
+  }
+
+  /**
+   * @param {GlobalTransform2D} target
+   */
+  static clone(target) {
+    return this.copy(target)
+  }
+}
