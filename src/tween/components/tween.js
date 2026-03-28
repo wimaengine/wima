516e6b4d5146a5ac5c242b1d3e574682698835ef
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2026-03-28 20:38:19 +0300

    Add static copy and clone methods on `Tween`

--- src/tween/components/tween.js
+++ src/tween/components/tween.js
@@ -54,4 +54,46 @@ export class Tween {
     this.duration = duration
     this.easing = easing
   }
+
+  /**
+   * @template U
+   * @param {Tween<U>} source
+   * @param {Tween<U>} target
+   */
+  static copy(source, target = new this(source.to, source.from, source.duration, source.repeat, source.flip, source.easing)) {
+    const cloneValue = (/** @type {U} */ value) => {
+      if (!value || typeof value !== 'object') {
+        return value
+      }
+
+      const ctor = value.constructor
+
+      // @ts-ignore
+      if (ctor && typeof ctor.copy === 'function') {
+        // @ts-ignore
+        return ctor.copy(value)
+      }
+
+      throw `The source cannot be copied with value type \`${ctor.name}\``
+    }
+
+    target.duration = source.duration
+    target.finish = source.finish
+    target.to = cloneValue(source.to)
+    target.from = cloneValue(source.from)
+    target.easing = source.easing
+    target.timeTaken = source.timeTaken
+    target.repeat = source.repeat
+    target.flip = source.flip
+
+    return target
+  }
+
+  /**
+   * @template U
+   * @param {Tween<U>} target
+   */
+  static clone(target) {
+    return this.copy(target)
+  }
 }
