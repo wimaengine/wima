952e656a15cddb0305831644ea44d31a154fd485
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2026-03-28 20:38:17 +0300

    Add static copy and clone methods on `AnimationTarget`

--- src/animation/components/target.js
+++ src/animation/components/target.js
@@ -20,4 +20,22 @@ export class AnimationTarget {
     this.player = player
     this.id = id
   }
+
+  /**
+   * @param {AnimationTarget} source
+   * @param {AnimationTarget} target
+   */
+  static copy(source, target = new AnimationTarget(source.player, source.id)) {
+    target.player = source.player
+    target.id = source.id
+
+    return target
+  }
+
+  /**
+   * @param {AnimationTarget} target
+   */
+  static clone(target) {
+    return this.copy(target)
+  }
 }
