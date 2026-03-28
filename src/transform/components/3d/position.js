2b2a6f8b9b2eccabc10372fd1cb6fac2d1bd8a0e
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2026-03-28 20:38:19 +0300

    Add static copy and clone methods on `Position3D`

--- src/transform/components/3d/position.js
+++ src/transform/components/3d/position.js
@@ -1,3 +1,19 @@
 import { Vector3 } from '../../../math/index.js'
 
-export class Position3D extends Vector3 {}
+export class Position3D extends Vector3 {
+
+  /**
+   * @param {Position3D} source
+   * @param {Position3D} target
+   */
+  static copy(source, target = new Position3D()) {
+    return super.copy(source, target)
+  }
+
+  /**
+   * @param {Position3D} target
+   */
+  static clone(target) {
+    return this.copy(target)
+  }
+}
