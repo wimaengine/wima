9e73048152fb41abca3f304e50496135c1bf0788
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2026-03-29 10:33:04 +0300

    Add static copy and clone methods on `TweenRepeat`

--- src/tween/components/markers.js
+++ src/tween/components/markers.js
@@ -1,2 +1,19 @@
-export class TweenRepeat { }
+export class TweenRepeat {
+
+  /**
+   * @param {TweenRepeat} source
+   * @param {TweenRepeat} target
+   */
+  static copy(source, target = new TweenRepeat()) {
+    return target
+  }
+
+  /**
+   * @param {TweenRepeat} target
+   */
+  static clone(target) {
+    return this.copy(target)
+  }
+}
+
 export class TweenFlip { }
