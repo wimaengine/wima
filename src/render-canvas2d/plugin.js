078202fa3b03680dfb2558dcc3cf2ba42dec3c9e
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2026-05-27 16:30:01 +0300

    Integrate the system into plugin

--- src/render-canvas2d/plugin.js
+++ src/render-canvas2d/plugin.js
@@ -1,9 +1,9 @@
 import { App, Plugin } from '../app/index.js'
-import { AppSchedule } from '../core/index.js'
+import { AppSchedule, CoreSystems } from '../core/index.js'
 import { TextureCache, BasicMaterial } from '../render-core/index.js'
 import { renderBasicMaterial } from './core/index.js'
 import { Canvas2DMaterialPlugin } from './plugins/index.js'
-import { registerCanvas2DTypes } from './systems/index.js'
+import { clearCanvas2d, registerCanvas2DTypes } from './systems/index.js'
 
 export class Canvas2DRendererPlugin extends Plugin {
 
@@ -14,6 +14,11 @@ export class Canvas2DRendererPlugin extends Plugin {
     app
       .setResource(new TextureCache())
       .registerSystem({ schedule: AppSchedule.Startup, system: registerCanvas2DTypes })
+      .registerSystem({
+        schedule: AppSchedule.Update,
+        systemGroup: CoreSystems.Start,
+        system: clearCanvas2d
+      })
       .registerPlugin(new Canvas2DMaterialPlugin({
         material:BasicMaterial,
         update:renderBasicMaterial
