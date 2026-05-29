62ab4fd6cbb444f7f71eb909c9585ad8f5798488
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2026-05-29 08:51:17 +0300

    Update audio examples

--- examples/samples/audio/audioGraph.js
+++ examples/samples/audio/audioGraph.js
@@ -10,8 +10,7 @@ import {
   Canvas2DRendererPlugin,
   DefaultPlugin,
   DOMWindowPlugin,
-  FPSDebugger,
-  Plugin
+  FPSDebugger
 } from 'wima'
 import { HackPlugin, setupViewport } from '../utils.js'
 
@@ -31,18 +30,6 @@ class Playing {
   }
 }
 
-// Why use a plugin? The current implentation does not have system sets and
-// systems registered directly on the App are precede ones registered by plugins.
-// This conflict is between internal engine system and external engine systems ordering.
-class MyPlugin extends Plugin {
-
-  /**
-   * @param {App} app
-   */
-  register(app) {
-    app.registerSystem({ schedule: AppSchedule.Startup, system: init })
-  }
-}
 const app = new App()
 
 app
@@ -50,7 +37,7 @@ app
   .registerPlugin(new DefaultPlugin())
   .registerPlugin(new DOMWindowPlugin())
   .registerPlugin(new Canvas2DRendererPlugin())
-  .registerPlugin(new MyPlugin())
+  .registerSystem({ schedule: AppSchedule.Startup, system: init })
   .registerSystem({ schedule: AppSchedule.Update, system: setupViewport })
   .registerSystem({ schedule: AppSchedule.Update, system: playAudio })
   .registerDebugger(new FPSDebugger())
--- examples/samples/audio/audioPlayer.js
+++ examples/samples/audio/audioPlayer.js
@@ -11,23 +11,9 @@ import {
   Canvas2DRendererPlugin,
   DefaultPlugin,
   DOMWindowPlugin,
-  FPSDebugger,
-  Plugin
+  FPSDebugger
 } from 'wima'
 import { HackPlugin, setupViewport } from '../utils.js'
-
-// Why use a plugin? The current implentation does not have system sets and
-// systems registered directly on the App are precede ones registered by plugins.
-// This conflict is between internal engine system and external engine systems ordering.
-class MyPlugin extends Plugin {
-
-  /**
-   * @param {App} app
-   */
-  register(app) {
-    app.registerSystem({ schedule: AppSchedule.Startup, system: init })
-  }
-}
 
 const app = new App()
 
@@ -36,7 +22,7 @@ app
   .registerPlugin(new DefaultPlugin())
   .registerPlugin(new DOMWindowPlugin())
   .registerPlugin(new Canvas2DRendererPlugin())
-  .registerPlugin(new MyPlugin())
+  .registerSystem({ schedule: AppSchedule.Startup, system: init })
   .registerSystem({ schedule: AppSchedule.Update, system: setupViewport })
   .registerDebugger(new FPSDebugger())
   .run()
