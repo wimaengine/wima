0ea8ea4d93261ac643f3ae551fe3ef7b25dc4952
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2026-06-10 10:06:00 +0300

    Add serialization/deserialization to `Entity`

--- src/ecs/entities/entity.js
+++ src/ecs/entities/entity.js
@@ -50,6 +50,38 @@ export class Entity {
 
     return new Entity(index, generation)
   }
+
+  /**
+   * @param {Entity} value
+   */
+  static serialize(value) {
+    return {
+      index: value.index,
+      generation: value.generation
+    }
+  }
+
+  /**
+   * @param {EntityId} value
+   * @param {Entity} [out]
+   */
+  static deserialize(value, out = new Entity(0, 0)) {
+    const [index, generation] = unpackFrom64Int(value)
+    const target = /** @type {any} */ (out)
+
+    target.index = index
+    target.generation = generation
+
+    return out
+  }
+
+  /**
+   * @param {unknown} value
+   * @returns {value is EntityId}
+   */
+  static validateSerial(value) {
+    return typeof value === 'number'
+  }
 }
 
 /**
