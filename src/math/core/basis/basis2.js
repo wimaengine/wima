4adad9cc394bac0b1af928a22304cd15380b8683
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2026-06-11 09:32:00 +0300

    Add serialization/deserialization to `Basis2`

--- src/math/core/basis/basis2.js
+++ src/math/core/basis/basis2.js
@@ -20,4 +20,50 @@ export class Basis2 {
     this.x = x
     this.y = y
   }
+
+  /**
+   * @param {Basis2} value
+   */
+  static serialize(value) {
+    return {
+      x: Vector2.serialize(value.x),
+      y: Vector2.serialize(value.y)
+    }
+  }
+
+  /**
+   * @param {Basis2Serial} value
+   * @param {Basis2} [out]
+   */
+  static deserialize(value, out = new Basis2()) {
+    out.x = Vector2.deserialize(value.x, out.x)
+    out.y = Vector2.deserialize(value.y, out.y)
+
+    return out
+  }
+
+  /**
+   * @param {unknown} value
+   * @returns {value is Basis2Serial}
+   */
+  static validateSerial(value) {
+    if (!value || typeof value !== 'object') {
+      return false
+    }
+
+    if (!('x' in value) || !('y' in value)) {
+      return false
+    }
+
+    return Vector2.validateSerial(value.x)
+      && Vector2.validateSerial(value.y)
+  }
 }
+
+/**
+ * Serialized form of `Basis2`.
+ *
+ * @typedef Basis2Serial
+ * @property {any} x
+ * @property {any} y
+ */
