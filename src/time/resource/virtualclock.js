436554e62ae41a8ba4a2348972135a5f0bcc7e3b
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2026-06-15 15:21:00 +0300

    Add serialization/deserialization to `VirtualClock`

--- src/time/resource/virtualclock.js
+++ src/time/resource/virtualclock.js
@@ -1,3 +1,18 @@
 import { Clock } from '../clock.js'
 
-export class VirtualClock extends Clock { }
+export class VirtualClock extends Clock {
+  /**
+   * @param {Clock} value
+   */
+  static serialize(value) {
+    return super.serialize(value)
+  }
+
+  /**
+   * @param {import('../clock.js').ClockSerial} value
+   * @param {VirtualClock} [out]
+   */
+  static deserialize(value, out = new VirtualClock()) {
+    return super.deserialize(value, out)
+  }
+}
