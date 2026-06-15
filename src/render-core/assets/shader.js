b0130ba68e192ddf2597b0f6ba6246a9386410b1
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2026-06-15 09:29:00 +0300

    Add serialization/deserialization to `Shader`

--- src/render-core/assets/shader.js
+++ src/render-core/assets/shader.js
@@ -24,4 +24,50 @@ export class Shader {
   static default() {
     return new Shader(ShaderStage.Fragment, '')
   }
+
+  /**
+   * @param {Shader} value
+   */
+  static serialize(value) {
+    return {
+      stage: value.stage,
+      source: value.source
+    }
+  }
+
+  /**
+   * @param {ShaderSerial} value
+   * @param {Shader} [out]
+   */
+  static deserialize(value, out = Shader.default()) {
+    const target = /** @type {any} */ (out)
+
+    target.stage = value.stage
+    target.source = value.source
+
+    return out
+  }
+
+  /**
+   * @param {unknown} value
+   * @returns {value is ShaderSerial}
+   */
+  static validateSerial(value) {
+    if (!value || typeof value !== 'object') {
+      return false
+    }
+
+    if (!('stage' in value) || !('source' in value)) {
+      return false
+    }
+
+    return typeof value.stage === 'number'
+      && typeof value.source === 'string'
+  }
 }
+
+/**
+ * @typedef ShaderSerial
+ * @property {number} stage
+ * @property {string} source
+ */
