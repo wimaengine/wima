09d8c17535af5f48c6a3ad9a2eef1db9d0bea3a3
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2026-06-16 13:32:00 +0300

    Add serialization/deserialization to `RemoteTransform3D`

--- src/transform/components/3d/remote.js
+++ src/transform/components/3d/remote.js
@@ -55,4 +55,67 @@ export class RemoteTransform3D {
   static clone(target) {
     return RemoteTransform3D.copy(target)
   }
+
+  /**
+   * @param {RemoteTransform3D} value
+   */
+  static serialize(value) {
+    return {
+      copyTranslation: value.copyTranslation,
+      copyOrientation: value.copyOrientation,
+      copyScale: value.copyScale,
+      entity: Entity.serialize(value.entity),
+      offsetTransform: Affine3.serialize(value.offsetTransform)
+    }
+  }
+
+  /**
+   * @param {unknown} value
+   * @returns {value is RemoteTransform3DSerial}
+   */
+  static validateSerial(value) {
+    if (!value || typeof value !== 'object') {
+      return false
+    }
+
+    if (!('copyTranslation' in value)
+      || !('copyOrientation' in value)
+      || !('copyScale' in value)
+      || !('entity' in value)
+      || !('offsetTransform' in value)
+    ) {
+      return false
+    }
+
+    return typeof value.copyTranslation === 'boolean'
+      && typeof value.copyOrientation === 'boolean'
+      && typeof value.copyScale === 'boolean'
+      && Entity.validateSerial(value.entity)
+      && Affine3.validateSerial(value.offsetTransform)
+  }
+
+  /**
+   * @param {RemoteTransform3DSerial} value
+   * @param {RemoteTransform3D} [out]
+   */
+  static deserialize(value, out = new RemoteTransform3D(new Entity(0,0))) {
+    out.copyTranslation = value.copyTranslation
+    out.copyOrientation = value.copyOrientation
+    out.copyScale = value.copyScale
+    out.entity = Entity.deserialize(value.entity, out.entity)
+    out.offsetTransform = Affine3.deserialize(value.offsetTransform, out.offsetTransform)
+
+    return out
+  }
 }
+
+/**
+ * Serialized form of `RemoteTransform3D`.
+ *
+ * @typedef RemoteTransform3DSerial
+ * @property {boolean} copyTranslation
+ * @property {boolean} copyOrientation
+ * @property {boolean} copyScale
+ * @property {any} entity
+ * @property {any} offsetTransform
+ */
