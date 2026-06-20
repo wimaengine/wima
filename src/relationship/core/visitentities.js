cd8ee59a69dd8276c04d1d02047afe4fffde0e8a
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2026-06-20 13:12:50 +0300

    Add `VisitEntities.map`
    Maps entities from one entity space to another

--- src/relationship/core/visitentities.js
+++ src/relationship/core/visitentities.js
@@ -1,4 +1,5 @@
 import { Entity } from '../../ecs/index.js'
+import { abstractMethod } from '../../utils/errors.js'
 
 /**
  * @interface
@@ -10,6 +11,14 @@ export class VisitEntities {
    * @throws {string} Throws when the method is not implemented on implementing class.
    */
   visit() {
-    throw `The method \`${this.constructor.name}.visit\` is not implemented correctly.`
+    throw abstractMethod(this, VisitEntities, this.visit)
+  }
+
+  /**
+   * @param {Map<import('../../ecs/index.js').EntityId, import('../../ecs/index.js').EntityId>} _entityMap
+   * @throws {string} Throws when the method is not implemented on implementing class.
+   */
+  map(_entityMap) {
+    throw abstractMethod(this, VisitEntities, this.map)
   }
 }
