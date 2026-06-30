d951b00d11d513de931da7dfa5a198b421418b69
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2026-06-30 20:47:08 +0300

    Update load json scene example

--- examples/samples/scene/2d/load_json.js
+++ examples/samples/scene/2d/load_json.js
@@ -2,36 +2,17 @@ import {
   App,
   AppSchedule,
   AssetServer,
-  BasicMaterial,
-  BasicMaterialAssets,
   Canvas2DRendererPlugin,
-  Color,
   createTransform2D,
   DefaultPlugin,
   DOMWindowPlugin,
   EntityCommands,
   FPSDebugger,
-  Mesh,
-  MeshAssets,
   Scene,
   SceneInstance,
   World
 } from 'wima'
 import { HackPlugin, setupViewport } from '../../utils.js'
-
-// NOTE: This is here because scenes dont support resources nor assets.
-// Also asset dependencies need to be supported too in order to load
-// the external dependencies from the server/disk.
-// when that lands, this example should support them natively.
-// TODO: Move this to use internal scene assets when the above land
-class SceneAssetHandles {
-
-  /** @type {import('wima').Handle<Mesh> | undefined} */
-  meshHandle
-
-  /** @type {import('wima').Handle<BasicMaterial> | undefined} */
-  materialHandle
-}
 
 const app = new App()
 
@@ -50,16 +31,7 @@ app
  */
 function init(world) {
   const commands = new EntityCommands(world)
-  const meshes = world.getResource(MeshAssets)
-  const materials = world.getResource(BasicMaterialAssets)
   const server = world.getResource(AssetServer)
-  const handles = new SceneAssetHandles()
-
-  handles.meshHandle = meshes.add(Mesh.circle2D(0.18, 14))
-  handles.materialHandle = materials.add(new BasicMaterial({
-    color: new Color(0.95, 0.8, 0.2)
-  }))
-  world.setResource(handles)
 
   const sceneHandle = server.load(Scene, '/scene/basic.json')
 
