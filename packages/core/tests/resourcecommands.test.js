037f87bffbbc989c09b523a2b103c6100e05b11d
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2026-08-17 23:08:09 +0300

    Add resource commands tests

--- packages/core/tests/resourcecommands.test.js
+++ packages/core/tests/resourcecommands.test.js
@@ -3,8 +3,7 @@ import { describe, test } from 'vitest'
 import { CommandQueue } from '@wimaengine/command'
 import { World } from '@wimaengine/ecs'
 import { typeid } from '@wimaengine/type'
-import { ResourceCommands } from '../src/index.js'
-import { executeCommands } from '../src/systems/index.js'
+import { executeCommands, ResourceCommands } from '../src/index.js'
 
 class TestResource { }
 class TestAlias extends TestResource { }
